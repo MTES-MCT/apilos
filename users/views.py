ef485ee0a0d5ccdd854e76038374aa19580c7a63
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2021-07-23 11:39:53 +0200

    gestion de l'erreur de login et du formulaire reset password avec le système de design de l'état (#3)
    * add a linter
    
    * ignorer les confg local vscode
    
    * gestion d'erreur lors du login et suppression des boutons se loguer et espace instructeur sur la page du login
    
    * gestion du redirect après logout et du reset de mot de passe
    
    * fix tests because of namespace

--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 
 def home(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('conventions:index') )
+    # test si authentifié, si oui, rediriger vers convention/index...
     return render(request, "index.html")
