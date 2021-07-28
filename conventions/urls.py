eacd9b8053c4cc583174746aecc6825ddcbb002c
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2021-07-28 12:59:00 +0200

    premier caneva de navigation pour le formulaire de création de conventions

--- conventions/urls.py
+++ conventions/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
   path('', views.index, name='index'),
+  path('step1', views.step1, name='step1'),
+  path('step2', views.step2, name='step2'),
+  path('step3', views.step3, name='step3'),
+  path('step4', views.step4, name='step4'),
+  path('step5', views.step5, name='step5'),
+  path('step6', views.step6, name='step6'),
+  path('step7', views.step7, name='step7'),
+  path('step8', views.step8, name='step8'),
+  path('stepfin', views.stepfin, name='stepfin'),
 ]
--- conventions/views.py
+++ conventions/views.py
@@ -11,3 +11,22 @@ convention_service = ConventionService()
 def index(request):
     conventions = convention_service.all_conventions(request.user)
     return render(request, "conventions/index.html", {'conventions': conventions})
+
+def step1(request):
+    return render(request, "conventions/step1.html")
+def step2(request):
+    return render(request, "conventions/step2.html")
+def step3(request):
+    return render(request, "conventions/step3.html")
+def step4(request):
+    return render(request, "conventions/step4.html")
+def step5(request):
+    return render(request, "conventions/step5.html")
+def step6(request):
+    return render(request, "conventions/step6.html")
+def step7(request):
+    return render(request, "conventions/step7.html")
+def step8(request):
+    return render(request, "conventions/step8.html")
+def stepfin(request):
+    return render(request, "conventions/stepfin.html")
