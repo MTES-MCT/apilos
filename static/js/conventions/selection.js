2ddaeb4c14aac0d54119cfa5ec99d3877671cc1c
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2021-10-20 15:57:41 +0200

    fix total of logements in recap (#72)

--- static/js/conventions/selection.js
+++
@@ -1,8 +0,0 @@
-function load_selection_page() {
-  document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('selection')
-            .addEventListener('click', convention_mode(true));
-  });
-}
-
-load_selection_page()
