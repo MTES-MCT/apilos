de0d91e3575740fae27b8e87c3fcf87405f7fa4e
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2021-10-20 15:51:18 +0200

    fix total of logements in recap

--- static/js/recap-logements-table.js
+++ static/js/recap-logements-table.js
@@ -1,14 +1,8 @@
-function compute_total_value(what, is_form=true){
+function compute_total_value(what, is_form){
     elements = document.querySelectorAll('[id=lgt_' + what + ']');
     total_value = 0;
     for ( i =0, len = elements.length; i<len; i++) {
-        if (is_form) {
-            total_value = total_value + parseFloat(elements[i].getElementsByTagName('input')[0].value);
-        }
-        else {
-            total_value = total_value + parseFloat(elements[i].innerHTML);
-        }
-        parseFloat(elements[i].innerHTML)
+        total_value = total_value + parseFloat(elements[i].innerHTML);
     }
     document.getElementById('total_' + what).innerHTML = "<span>" + total_value.toFixed(2).replace('.',',').replace(/\B(?=(\d{3})+(?!\d))/g, " ") + "</span>";
 }
