01a9c1905626cef9e3ae912b333c392fe5fdd2bc
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-01-06 11:54:57 +0100

    Add bailleur_id to conventions API

--- conventions/api/api_views.py
+++ conventions/api/api_views.py
@@ -5,7 +5,12 @@ from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+# from django_filters.rest_framework import DjangoFilterBackend
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+
 from api.csrf_exempt_session_authentication import CsrfExemptSessionAuthentication
+from bailleurs.models import Bailleur
 from conventions.models import Convention
 from conventions.api.serializers import ConventionSerializer
 from conventions.api.permissions import ConventionPermission
@@ -23,14 +28,47 @@ class ConventionList(
     http_method_names = ["get", "head"]
 
     serializer_class = ConventionSerializer
+    # filter_backends = [DjangoFilterBackend]
+    # filterset_fields = [
+    #     "bailleur",
+    #     # "progamme__administration__uuid"
+    # ]
+
+    bailleur_uuid_param = openapi.Parameter(
+        "bailleur_uuid",
+        openapi.IN_QUERY,
+        description="UUID du bailleur",
+        required=False,
+        type=openapi.TYPE_STRING,
+    )
+
+    # progamme__administration__uuid_param = openapi.Parameter(
+    #     "progamme__administration__uuid",
+    #     openapi.IN_QUERY,
+    #     description="UUID d l'administration",
+    #     required=False,
+    #     type=openapi.TYPE_STRING,
+    # )
 
     def get_queryset(self):
         """
         This view should return a list of all the purchases
         for the currently authenticated user.
         """
-        return self.request.user.conventions()
+        convention_filter = {}
+        if "bailleur_uuid" in self.request.query_params:
+            bailleur = Bailleur.objects.get(
+                uuid=self.request.query_params["bailleur_uuid"]
+            )
+            convention_filter["bailleur_id"] = bailleur.id
+        return self.request.user.conventions().filter(**convention_filter)
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            bailleur_uuid_param,
+            # progamme__administration__uuid_param,
+        ]
+    )
     def get(self, request):  # , format=None):
         return self.list(request)  # , *args, **kwargs)
 
