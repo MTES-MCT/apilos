6867038fb8654614e2c440759394441a5c824860
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-02-05 14:46:24 +0100

    fix total of logements in recapitulatif

--- static/js/recap-logements-table.js
+++ static/js/recap-logements-table.js
@@ -2,7 +2,7 @@ function compute_total_value(what, is_form){
     elements = document.querySelectorAll('[id=lgt_' + what + ']');
     total_value = 0;
     for ( i =0, len = elements.length; i<len; i++) {
-        total_value = total_value + parseFloat(elements[i].innerHTML);
+        total_value = total_value + parseFloat(elements[i].innerHTML.replace(',','.'));
     }
     document.getElementById('total_' + what).innerHTML = "<span>" + total_value.toFixed(2).replace('.',',').replace(/\B(?=(\d{3})+(?!\d))/g, " ") + "</span>";
 }
