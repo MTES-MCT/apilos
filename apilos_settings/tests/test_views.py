3d1bf3abc7bd8f5b2d924c4d763a7132dd75b289
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-02-21 11:58:40 +0100

    display bailleur type in bailleur form, editable only for superuser (#144)

---
+++ apilos_settings/tests/test_views.py
@@ -0,0 +1,51 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from core.tests import utils_fixtures
+
+
+class ApilosSettingsViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        utils_fixtures.create_all()
+
+    def test_display_bailleurs(self):
+        """
+        Superuser will display Profil, Administrations, Bailleurs and Utilisateurs in sidemenu
+        Instructeur will display Profil, Administrations and Utilisateurs in sidemenu
+        Bailleur will display Profil, Bailleurs and Utilisateurs in sidemenu
+        """
+
+        response = self.client.post(
+            reverse("login"), {"username": "nicolas", "password": "12345"}
+        )
+        response = self.client.get(reverse("settings:index"))
+        self.assertRedirects(response, reverse("settings:users"))
+        response = self.client.get(reverse("settings:users"))
+        self.assertContains(response, "Votre profil")
+        self.assertContains(response, "Administrations")
+        self.assertContains(response, "Bailleurs")
+        self.assertContains(response, "Utilisateurs")
+
+        response = self.client.post(
+            reverse("login"), {"username": "sabine", "password": "12345"}
+        )
+
+        response = self.client.get(reverse("settings:index"))
+        self.assertRedirects(response, reverse("settings:administrations"))
+        response = self.client.get(reverse("settings:administrations"))
+        self.assertContains(response, "Votre profil")
+        self.assertContains(response, "Administrations")
+        self.assertContains(response, "Utilisateurs")
+        self.assertNotContains(response, "Bailleurs")
+
+        response = self.client.post(
+            reverse("login"), {"username": "raph", "password": "12345"}
+        )
+        response = self.client.get(reverse("settings:index"))
+        self.assertRedirects(response, reverse("settings:bailleurs"))
+        response = self.client.get(reverse("settings:bailleurs"))
+        self.assertContains(response, "Votre profil")
+        self.assertContains(response, "Bailleurs")
+        self.assertContains(response, "Utilisateurs")
+        self.assertNotContains(response, "Administrations")
