78ed527f87e614fadb8ff376f8668272ae77a107
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-03-26 23:17:30 +0100

    manage new icon form and color to be more explicit

--- static/js/comment-factory.js
+++ static/js/comment-factory.js
@@ -21,19 +21,15 @@ class CommentFactory {
 
     // add the icon in the container
     _add_comment_icon() {
-        this.container.innerHTML = '\
-<div id="' + this.comment_icon_id + '" class="content__icons--darkgrey" hidden>\
-    <div class="content__icons"\
-        id="' + this.comment_icon_id + '-img"\
-        data-fr-opened="false"\
-        aria-controls="' + this.comment_dialog_id + '-dialog">\
-        <svg role="img" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewbox="0 0 24 24" aria-labelledby="chatIconTitle">\
-            <title id="chatIconTitle">Annexes comments</title>\
-            <path d="M8.82388455,18.5880577 L4,21 L4.65322944,16.4273939 C3.00629211,15.0013 2,13.0946628 2,11 C2,6.581722 6.4771525,3 12,3 C17.5228475,3 22,6.581722 22,11 C22,15.418278 17.5228475,19 12,19 C10.8897425,19 9.82174472,18.8552518 8.82388455,18.5880577 Z"></path>\
-        </svg>\
-    </div>\
-</div>\
-'
+        const icon_div = document.createElement('div');
+        icon_div.setAttribute('id', this.comment_icon_id)
+        icon_div.setAttribute('data-fr-opened', 'false')
+        icon_div.setAttribute('aria-controls', this.comment_dialog_id + '-dialog')
+        icon_div.classList.add('content__icons')
+        icon_div.classList.add('content__icons--darkgrey')
+        icon_div.hidden = true
+        this.container.appendChild(icon_div)
+
         if (document.getElementById(this.comment_dialog_id) === null) {
             //create dialog: Can be improved
             const dialog_modal = document.createElement('dialog');
