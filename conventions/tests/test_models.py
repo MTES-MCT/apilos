91b8f6ec1e31c10706d8a99e71b55a26dfaee56a
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-04-12 17:21:32 +0200

    add and organize tests for pret model

--- conventions/tests/test_models.py
+++ conventions/tests/test_models.py
@@ -18,17 +18,6 @@ class ConventionModelsTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         utils_fixtures.create_all()
-        convention = Convention.objects.get(numero="0001")
-        Pret.objects.create(
-            convention=convention,
-            bailleur=convention.bailleur,
-            preteur=Preteur.CDCF,
-            date_octroi=datetime.datetime.today(),
-            autre="test autre",
-            numero="mon numero",
-            duree=50,
-            montant=123456.789,
-        )
 
     def test_object_str(self):
         convention = Convention.objects.get(numero="0001")
@@ -67,32 +56,6 @@ class ConventionModelsTest(TestCase):
         self.assertFalse(convention.is_instructeur_editable())
         self.assertFalse(convention.is_instruction_ongoing())
         self.assertFalse(convention.is_project())
-
-    def test_properties(self):
-        pret = Pret.objects.first()
-        self.assertEqual(pret.get_preteur_display(), pret.p)
-        self.assertEqual(pret.autre, pret.a)
-        self.assertEqual(pret.date_octroi, pret.do)
-        self.assertEqual(pret.numero, pret.n)
-        self.assertEqual(pret.duree, pret.d)
-        self.assertEqual(pret.montant, pret.m)
-
-    def test_p_full(self):
-        pret = Pret.objects.first()
-        pret.preteur = Preteur.CDCF
-        self.assertEqual(
-            pret.p_full(), "Caisse de Dépôts et Consignation pour le foncier"
-        )
-        pret.preteur = Preteur.CDCL
-        self.assertEqual(
-            pret.p_full(), "Caisse de Dépôts et Consignation pour le logement"
-        )
-        pret.preteur = Preteur.AUTRE
-        self.assertEqual(pret.p_full(), "Autre")
-        pret.preteur = Preteur.ETAT
-        self.assertEqual(pret.p_full(), "Etat")
-        pret.preteur = Preteur.REGION
-        self.assertEqual(pret.p_full(), "Région")
 
     def test_get_email_bailleur_users(self):
         convention = Convention.objects.get(numero="0001")
@@ -201,3 +164,57 @@ class ConventionModelsTest(TestCase):
 
     def test_xlsx(self):
         utils_assertions.assert_xlsx(self, Pret, "financement")
+
+
+class PretModelsTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        utils_fixtures.create_all()
+        convention = Convention.objects.get(numero="0001")
+        Pret.objects.create(
+            convention=convention,
+            bailleur=convention.bailleur,
+            preteur=Preteur.CDCF,
+            date_octroi=datetime.datetime.today(),
+            autre="test autre",
+            numero="mon numero",
+            duree=50,
+            montant=123456.789,
+        )
+
+    def test_p_full(self):
+        pret = Pret.objects.first()
+        pret.preteur = Preteur.CDCF
+        self.assertEqual(
+            pret.p_full(), "Caisse de Dépôts et Consignation pour le foncier"
+        )
+        pret.preteur = Preteur.CDCL
+        self.assertEqual(
+            pret.p_full(), "Caisse de Dépôts et Consignation pour le logement"
+        )
+        pret.preteur = Preteur.AUTRE
+        self.assertEqual(pret.p_full(), "Autre")
+        pret.preteur = Preteur.ETAT
+        self.assertEqual(pret.p_full(), "Etat")
+        pret.preteur = Preteur.REGION
+        self.assertEqual(pret.p_full(), "Région")
+
+    def test_properties(self):
+        pret = Pret.objects.first()
+        self.assertEqual(pret.get_preteur_display(), pret.p)
+        self.assertEqual(pret.autre, pret.a)
+        self.assertEqual(pret.date_octroi, pret.do)
+        self.assertEqual(pret.numero, pret.n)
+        self.assertEqual(pret.duree, pret.d)
+        self.assertEqual(pret.montant, pret.m)
+
+    def test_preteur_display(self):
+        pret = Pret.objects.first()
+
+        for k, _ in Preteur.choices:
+            if k != Preteur.AUTRE:
+                pret.preteur = k
+                self.assertEqual(pret.p_full(), pret.preteur_display())
+        pret.preteur = Preteur.AUTRE
+        pret.autre = "n'importe quoi"
+        self.assertEqual(pret.preteur_display(), "n'importe quoi")
