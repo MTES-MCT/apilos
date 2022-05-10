282c9891ac2e925eefe70f2e636c7db64172c5a4
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-05-10 09:53:59 +0200

    add config route to api-siap endpoint (#181)

---
+++ core/api/api_views.py
@@ -0,0 +1,104 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.renderers import JSONRenderer
+from rest_framework import serializers
+from drf_spectacular.utils import (
+    extend_schema,
+    OpenApiResponse,
+    OpenApiExample,
+    inline_serializer,
+)
+
+from django.conf import settings
+
+from api.csrf_exempt_session_authentication import CsrfExemptSessionAuthentication
+
+
+class ApilosConfiguration(APIView):
+    """
+    return the main configutations of the application
+    """
+
+    authentication_classes = [CsrfExemptSessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    renderer_classes = [JSONRenderer]
+
+    @extend_schema(
+        tags=["config-resource"],
+        responses={
+            200: inline_serializer(
+                name="APiLosConfiguration",
+                fields={
+                    "racine_url_acces_web": serializers.CharField(
+                        max_length=2000,
+                        help_text="Racine de l'URL d'accès web à l'application avec son protocole",
+                        #   example: https://staging.apilos.incubateur.net
+                    ),
+                    "url_acces_web_operation": serializers.CharField(
+                        max_length=2000,
+                        help_text=(
+                            "URL d'accès web à l'application sur la page d'une opération"
+                            + " (en relatif)"
+                        ),
+                        #   example: /operations/<NUM_OPE_SIAP>
+                    ),
+                    "url_acces_web_recherche": serializers.CharField(
+                        max_length=2000,
+                        help_text=(
+                            "URL d'accès web à la page de recherche des conventions"
+                            + " (en relatif)"
+                        ),
+                        #   example: /conventions
+                    ),
+                    "version": serializers.CharField(
+                        max_length=20,
+                        help_text=(
+                            "version API x.y, La version majeure sera indentée seulement en cas"
+                            + " de non compatibilité"
+                        )
+                        #   example: v1.2
+                    ),
+                },
+            ),
+            400: OpenApiResponse(description="Bad request (something invalid)"),
+            401: OpenApiResponse(description="Unauthorized"),
+            403: OpenApiResponse(description="Forbidden"),
+            404: OpenApiResponse(description="Not Found"),
+        },
+        examples=[
+            OpenApiExample(
+                "Configuration",
+                summary="Example of returned configuration",
+                description="Example of returned configuration when /config url is called",
+                value={
+                    "racine_url_acces_web": "https://apilos.beta.gouv.fr",
+                    "url_acces_web_operation": "/operations/{NUMERO_OPEPERATION_SIAP}",
+                    "url_acces_web_recherche": "/conventions",
+                    "version": "0.1",
+                },
+                request_only=False,  # signal that example only applies to requests
+                response_only=True,  # signal that example only applies to responses
+            ),
+        ],
+    )
+    def get(self, request):
+        """
+        Return main settings of the application.
+        """
+        if request.is_secure():
+            protocol = "https://"
+        else:
+            protocol = "http://"
+        version = ".".join(settings.SPECTACULAR_SETTINGS["VERSION"].split(".")[:-1])
+
+        return Response(
+            {
+                "racine_url_acces_web": protocol + request.get_host(),
+                "url_acces_web_operation": "/operations/{NUMERO_OPEPERATION_SIAP}",
+                "url_acces_web_recherche": "/conventions",
+                "version": version,
+            }
+        )
