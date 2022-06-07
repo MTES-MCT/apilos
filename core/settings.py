dfc22117786ec129d36f27abefd721412357acc6
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-06-08 00:42:53 +0200

    update Procfile to start worker in scalingo

--- core/settings.py
+++ core/settings.py
@@ -380,15 +380,6 @@ DRAMATIQ_BROKER = {
         "url": config("REDIS_URL"),
     },
     "MIDDLEWARE": [
-        # "dramatiq.middleware.AgeLimit",
-        # "dramatiq.middleware.TimeLimit",
-        # "dramatiq.middleware.Callbacks",
-        # "dramatiq.middleware.Retries",
-        # "django_dramatiq.middleware.DbConnectionsMiddleware",
         "django_dramatiq.middleware.AdminMiddleware",
     ],
 }
-
-# Defines which database should be used to persist Task objects when the
-# AdminMiddleware is enabled.  The default value is "default".
-DRAMATIQ_TASKS_DATABASE = "default"
