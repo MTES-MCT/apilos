494c6882d7e6c8ab67689914b0b1238d7854945d
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-06-14 11:24:50 +0200

    S434 Display headers and manu following SIAP habilitation (#214)

--- core/context_processor.py
+++ core/context_processor.py
@@ -1,3 +1,4 @@
+from core.siap_client.client import SIAPClient
 from core import settings
 
 
@@ -5,4 +6,9 @@ def get_environment(request):
     data = {}
     data["ENVIRONMENT"] = settings.ENVIRONMENT
     data["CERBERE_AUTH"] = settings.CERBERE_AUTH
+    if settings.CERBERE_AUTH:
+        client = SIAPClient.get_instance()
+        data["RACINE_URL_ACCES_WEB"] = client.racine_url_acces_web
+        data["URL_ACCES_WEB"] = client.url_acces_web
+        data["URL_ACCES_WEB_OPERATION"] = client.url_acces_web_operation
     return data
