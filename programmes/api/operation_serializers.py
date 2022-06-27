502278f362f6eb168f82d3261d9052e3eeeab055
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-06-27 10:47:22 +0200

    remove useless fields in sip API

--- programmes/api/operation_serializers.py
+++ programmes/api/operation_serializers.py
@@ -10,19 +10,14 @@ class BailleurSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Bailleur
         fields = (
-            "uuid",
             "nom",
+            "siren",
             "siret",
             "adresse",
             "code_postal",
             "ville",
             "capital_social",
-            "signataire_nom",
-            "signataire_fonction",
-            "signataire_date_deliberation",
             "type_bailleur",
-            "cree_le",
-            "mis_a_jour_le",
         )
         ref_name = "Bailleur"
 
@@ -43,12 +38,9 @@ class LotSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Lot
         fields = (
-            "uuid",
             "nb_logements",
             "financement",
             "type_habitat",
-            "edd_volumetrique",
-            "edd_classique",
         )
         ref_name = "Lot"
 
@@ -59,18 +51,12 @@ class ConventionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Convention
         fields = (
-            "cree_le",
             "date_fin_conventionnement",
             "financement",
             "fond_propre",
             "lot",
-            "mis_a_jour_le",
             "numero",
-            "premiere_soumission_le",
-            "soumis_le",
             "statut",
-            "uuid",
-            "valide_le",
         )
         ref_name = "Convention"
 
@@ -91,28 +77,12 @@ class OperationSerializer(serializers.HyperlinkedModelSerializer):
             "ville",
             "adresse",
             "numero_galion",
-            "annee_gestion_programmation",
             "zone_123_bis",
             "zone_abc_bis",
-            "surface_utile_totale",
             "type_operation",
             "anru",
-            "nb_locaux_commerciaux",
-            "nb_bureaux",
-            "autres_locaux_hors_convention",
-            "permis_construire",
             "date_achevement_previsible",
             "date_achat",
             "date_achevement",
-            "vendeur",
-            "acquereur",
-            "reference_notaire",
-            "reference_publication_acte",
-            "acte_de_propriete",
-            "certificat_adressage",
-            "effet_relatif",
-            "reference_cadastrale",
-            "edd_volumetrique",
-            "edd_classique",
         )
         ref_name = "Operation"
