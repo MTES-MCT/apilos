ca271a3b5cadc19d4e15993a18b46aae0439c295
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-08-19 10:54:05 +0200

    s524: update article number of surface habitable

--- programmes/models.py
+++ programmes/models.py
@@ -489,7 +489,7 @@ class Logement(models.Model):
     import_mapping = {
         "Désignation des logements": designation,
         "Type": typologie,
-        "Surface habitable\n(article R.111-2)": surface_habitable,
+        "Surface habitable\n(article": surface_habitable,
         "Surface des annexes\nRéelle": surface_annexes,
         "Surface des annexes\nRetenue dans la SU": surface_annexes_retenue,
         "Surface utile\n(surface habitable augmentée de 50% de la surface des annexes)": (
