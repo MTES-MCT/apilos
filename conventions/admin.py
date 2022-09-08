3a595f0808c938a9b42ee72023e6402d5b6ecd40
Marjolaine Le Bon <marjolaine@happy-dev.fr>
2022-09-08 12:09:40 +0200

    add uuid field on admin list of convention and a search bar

--- conventions/admin.py
+++ conventions/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 
 from .models import Convention, Pret
 
-admin.site.register(Convention)
+
+class ConventionAdmin(admin.ModelAdmin):
+    list_display = ("view_programme", "uuid")
+    search_fields = ["programme__ville", "programme__nom", "uuid"]
+
+    # pylint: disable=R0201
+    @admin.display(description="Programme")
+    def view_programme(self, obj):
+        return (
+            f"{obj.programme.ville} -  {obj.programme.nom} - "
+            + f"{obj.lot.nb_logements} lgts - "
+            + f"{obj.lot.get_type_habitat_display()} - "
+            + f"{obj.lot.financement}"
+        )
+
+
+admin.site.register(Convention, ConventionAdmin)
 admin.site.register(Pret)
