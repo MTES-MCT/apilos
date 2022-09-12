a62f9ef6d297cd71898c3320ca205ed93a9d305f
Marjolaine <marjolaine@happy-dev.fr>
2022-09-12 12:37:21 +0200

    add uuid field on admin list of convention and a search bar (#293)
    * add uuid field on admin list of convention and a search bar
    
    * enhance Convention admin
    
    * deprecate get_field_key
    
    Co-authored-by: Nicolas Oudard <nicolas.oudard@tinyclues.com>

--- conventions/admin.py
+++ conventions/admin.py
@@ -2,5 +2,53 @@ from django.contrib import admin
 
 from .models import Convention, Pret
 
-admin.site.register(Convention)
+
+@admin.display(description="Programme")
+def view_programme(convention):
+    return (
+        f"{convention.programme.ville} -  {convention.lot} - "
+        + f"{convention.lot.nb_logements} lgts - "
+        + f"{convention.lot.get_type_habitat_display()}"
+    )
+
+
+class ConventionAdmin(admin.ModelAdmin):
+    list_display = (view_programme, "administration", "bailleur", "financement", "uuid")
+    search_fields = [
+        "programme__ville",
+        "programme__nom",
+        "financement",
+        "uuid",
+        "bailleur__nom",
+        "programme__administration__nom",
+    ]
+    fields = (
+        "administration",
+        "bailleur",
+        "programme",
+        "lot",
+        "numero",
+        "date_fin_conventionnement",
+        "financement",
+        "fond_propre",
+        "comments",
+        "statut",
+        "soumis_le",
+        "premiere_soumission_le",
+        "valide_le",
+        "avenant_type",
+        "donnees_validees",
+        "nom_fichier_signe",
+        "televersement_convention_signee_le",
+        "date_resiliation",
+    )
+    list_select_related = (
+        "programme",
+        "lot",
+        "bailleur",
+    )
+    readonly_fields = ("bailleur", "programme", "lot", "administration")
+
+
+admin.site.register(Convention, ConventionAdmin)
 admin.site.register(Pret)
