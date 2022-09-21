9f12d15339211493338ecdab9d659135e4b8494c
Pierre Lemée <pierre.lemee@beta.gouv.fr>
2022-09-21 19:14:53 +0200

    Create ecoloweb app + command & delegate import to service

---
+++ ecoloweb/management/commands/import_ecoloweb_database.py
@@ -0,0 +1,44 @@
+import sys
+from django.core.management import BaseCommand
+from django.db import connections, DatabaseError
+from django.db import transaction
+from ecoloweb.services import EcolowebImportService
+
+
+class Command(BaseCommand):
+    help = 'Download data from an ecoloweb database'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.importer = EcolowebImportService()
+
+    def add_arguments(self, parser):
+        parser.add_argument("--dry-run",
+                            action='store_true',
+                            help='Do not persist downloaded data'
+                            )
+
+    def handle(self, *args, **options):
+        if 'ecoloweb' not in connections:
+            print("No 'ecoloweb' connection defined, migration aborted!")
+            sys.exit(1)
+
+        dry_run = options["dry_run"]
+
+        if dry_run:
+            print("Running in dry mode")
+
+        transaction.set_autocommit(False)
+
+        try:
+            # Actual processing
+            self.importer.process()
+        except DatabaseError as e:
+            transaction.rollback()
+            transaction.rollback()
+            raise e
+        finally:
+            if dry_run:
+                transaction.rollback()
+            else:
+                transaction.commit()
