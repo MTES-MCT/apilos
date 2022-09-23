c003ce5a63696fdba2dda0de26270986baceec41
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-09-23 16:13:25 +0200

    make the requests to clear transactionals

--- programmes/management/commands/clear_all_data.py
+++ programmes/management/commands/clear_all_data.py
@@ -2,6 +2,7 @@ import logging
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from django.db import transaction
 
 from instructeurs.models import Administration
 from bailleurs.models import Bailleur
@@ -13,7 +14,7 @@ class Command(BaseCommand):
         if settings.ENVIRONMENT in ["production", "siap-production"]:
             logging.error("This action can't be called in production environment")
             return
-        print("Let's remove all")
-        Administration.objects.all().delete()
-        Bailleur.objects.all().delete()
-        User.objects.exclude(is_superuser=True).exclude(is_staff=True).delete()
+        with transaction.atomic():
+            Administration.objects.all().delete()
+            Bailleur.objects.all().delete()
+            User.objects.exclude(is_superuser=True).exclude(is_staff=True).delete()
