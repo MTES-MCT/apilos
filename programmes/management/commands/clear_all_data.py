ec7fea08530a7d445ea76984d33f7212c47f0415
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2022-10-04 14:43:13 +0200

    add info to clear_all_data command

--- programmes/management/commands/clear_all_data.py
+++ programmes/management/commands/clear_all_data.py
@@ -8,13 +8,18 @@ from instructeurs.models import Administration
 from bailleurs.models import Bailleur
 from users.models import User
 
+logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if settings.ENVIRONMENT in ["production", "siap-production"]:
-            logging.error("This action can't be called in production environment")
+            logger.error("This action can't be called in production environment")
             return
         with transaction.atomic():
             Administration.objects.all().delete()
+            logger.info("Administration were cleared")
             Bailleur.objects.all().delete()
+            logger.info("Bailleur were cleared")
             User.objects.exclude(is_superuser=True).exclude(is_staff=True).delete()
+            logger.info("non staff and superuser User were cleared")
