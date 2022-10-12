74b463fca7d605d0269d607d247398b64b4bad00
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2022-10-12 16:03:18 +0200

    Create AdministrationImportHandler

---
+++ ecoloweb/resources/sql/administrations.sql
@@ -0,0 +1,12 @@
+select
+    distinct on (eg.code)
+    eg.id,
+    eg.nom||' ('||eg.code||')' as nom, -- Workaround to avoid conflicts on duplicate names
+    eg.code,
+    ega.ville as ville_signature,
+    ega.ligne1||' '||ega.ligne2||' '||ega.ligne3||' '||ega.ligne4 as adresse,
+    ega.codepostal as code_postal,
+    ega.ville as ville
+from ecolo.ecolo_entitegest eg
+    left join ecolo.ecolo_entitegestadresse ega on eg.adresse_id = ega.id
+order by eg.code, eg.version desc
