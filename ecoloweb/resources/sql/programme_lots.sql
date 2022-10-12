2c4c8404db1a2312e5f5123f82c0ffaca540fe05
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2022-10-12 17:58:42 +0200

    Extra fields from programme lots query

--- ecoloweb/resources/sql/programme_lots.sql
+++ ecoloweb/resources/sql/programme_lots.sql
@@ -1,41 +1,52 @@
 -- Requête pour alimenter la table programmes_lot
 
--- financement varchar(25)  not null
--- type_habitat   enum('INDIVIDUEL', 'COLLECTIF', 'MIXTE')
--- cree_le timestamp with time zone not null
--- mis_a_jour_le  timestamp with time zone not null
--- bailleur_id  FK(bailleurs_bailleur) not null
--- programme_id FK(programmes_programme) not null
+-- Champs restants à mapper
 -- annexe_ateliers boolean not null
 -- annexe_balcons boolean not null
--- annexe_caves boolean not null
 -- annexe_celliers              boolean                  not null,
 -- annexe_combles               boolean                  not null,
 -- annexe_loggias               boolean                  not null,
--- annexe_remises               boolean                  not null,
 -- annexe_resserres             boolean                  not null,
 -- annexe_sechoirs              boolean                  not null,
 -- annexe_soussols              boolean                  not null,
--- annexe_terrasses             boolean                  not null,
 -- edd_classique                text,
 -- edd_volumetrique             text,
--- type_habitat                 varchar(25)              not null,
 -- lgts_mixite_sociale_negocies integer                  not null,
--- loyer_derogatoire            numeric(6, 2),
--- parent_id   FK(programmes_lot)
+-- parent_id   FK(programmes_lot) dans le cas d'un avenant seulement ?
 
 select
     pl.id,
     cdg as programme_id,
+    123456789 as bailleur_id, -- TODO link with actual data
+    coalesce(pl.financementdate, now()) as cree_le,
+    coalesce(pl.financementdate, now()) as mis_a_jour_le,
+    ff.code as financement,
     logementsnombretotal as nb_logements,
     case
         when coalesce(pl.logementsnombreindtotal, 0) > 0 and coalesce(pl.logementsnombrecoltotal, 0) > 0 then 'MIXTE'
         when coalesce(pl.logementsnombreindtotal, 0) > 0 then 'INDIVIDUEL'
         else 'COLLECTIF'
-    end as type_habitat
+    end as type_habitat,
+    ap1.id is not null as annexe_caves,
+    ap2.id is not null as annexe_terrasses,
+    ap3.id is not null as annexe_remises,
+    case
+        when pl.estderogationloyer and coalesce(pl.logementsnombreindtotal, 0) > 0 then pl.montantplafondloyerindinitial
+        when pl.estderogationloyer and coalesce(pl.logementsnombrecoltotal, 0) > 0 then pl.montantplafondloyercolinitial
+    end as loyer_derogatoire
 from ecolo.ecolo_programmelogement pl
     inner join ecolo.ecolo_conventiondonneesgenerales cdg on pl.conventiondonneesgenerales_id = cdg.id
     inner join ecolo.ecolo_conventionapl c on cdg.conventionapl_id = c.id
+    -- Financement
+    inner join ecolo.ecolo_typefinancement tf on pl.typefinancement_id = tf.id
+    inner join ecolo.ecolo_famillefinancement ff on tf.famillefinancement_id = ff.id
+    -- Annexes
+    left join ecolo.ecolo_annexe a1 on a1.programmelogement_id = pl.id
+    left join ecolo.ecolo_valeurparamstatic ap1 on a1.typeannexe_id = ap1.id and ap1.subtype = 'TAN' and ap1.code = 7 -- Cave
+    left join ecolo.ecolo_annexe a2 on a2.programmelogement_id = pl.id
+    left join ecolo.ecolo_valeurparamstatic ap2 on a2.typeannexe_id = ap2.id and ap2.subtype = 'TAN' and ap2.code = 5 -- Terrasse
+    left join ecolo.ecolo_annexe a3 on a3.programmelogement_id = pl.id
+    left join ecolo.ecolo_valeurparamstatic ap3 on a3.typeannexe_id = ap3.id and ap3.subtype = 'TAN' and ap3.code = 8 -- Box
 where
     cdg.avenant_id is null
     and pl.logementsnombretotal > 0
