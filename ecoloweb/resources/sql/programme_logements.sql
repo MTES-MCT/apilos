36f73b49772e80e46bfa058ca070be6e8e249786
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2022-10-12 18:31:16 +0200

    Create ProgrammeLogementImportHandler with SQL query

---
+++ ecoloweb/resources/sql/programme_logements.sql
@@ -0,0 +1,20 @@
+-- Requête pour alimenter la table programmes_logement
+
+-- Champs restants à mapper:
+-- surface_annexes_retenue numeric(6, 2)
+-- loyer_par_metre_carre   numeric(6, 2)
+-- bailleur_id  FK(bailleur)  not null
+-- lot_id   FK(programmes_lot) not null
+select
+    l.numerobatiment||' '||l.noescalier||' '||l.etage||' '||l.numerologement as designation,
+    ptl.code as typologie,
+    l.surfacehabitable::float as surface_habitable,
+    l.surfaceannexe::float as surface_annexes,
+    l.surfaceutile::float as surface_utile,
+    l.coefficientmodulation as coefficient,
+    l.montantloyer as loyer,
+    l.datecreation as cree_le,
+    l.datecreation as mis_a_jour_le,
+    l.programmelogement_id as lot_id
+from ecolo.ecolo_logement l
+    inner join ecolo.ecolo_valeurparamstatic ptl on l.typelogement_id = ptl.id
