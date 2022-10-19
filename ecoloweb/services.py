42806254e6e30ce9e19e5b2aed7c5a7a06a942d1
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2022-10-19 16:50:42 +0200

    Create BailleurImportHandler

--- ecoloweb/services.py
+++ ecoloweb/services.py
@@ -201,12 +201,36 @@ class AdministrationImportHandler(ModelImportHandler):
         if ref := self._find_ecolo_reference(Administration, ecolo_id) is None:
             administration, created = Administration.objects.get_or_create(code=data['code'], defaults=data)
         else:
+            print(
+                f"Skipping administration with ecolo id #{ecolo_id}, already imported ({ref.apilos_model} #{ref.apilos_id})")
             created = False
 
         return created
 
     def on_complete(self):
         print(f"Migrated {self.count} administration(s)")
+
+
+class BailleurImportHandler(ModelImportHandler):
+
+    def _get_sql_query(self) -> str:
+        return self._get_sql_from_template('resources/sql/bailleurs.sql', {'max_row': 10})
+
+    def _process_row(self, data: dict) -> bool:
+        ecolo_id = data.pop('id')
+
+        if ref := self._find_ecolo_reference(Bailleur, ecolo_id) is None:
+            bailleur, created = Bailleur.objects.get_or_create(siret=data['siret'], defaults=data)
+            self._register_ecolo_reference(bailleur, ecolo_id)
+        else:
+            print(
+                f"Skipping bailleur with ecolo id #{ecolo_id}, already imported ({ref.apilos_model} #{ref.apilos_id})")
+            created = False
+
+        return created
+
+    def on_complete(self):
+        print(f"Migrated {self.count} bailleur(s)")
 
 
 class EcolowebImportService:
@@ -224,7 +248,8 @@ class EcolowebImportService:
             #ProgrammeLotImportHandler(self.connection),
             #ProgrammeLogementImportHandler(self.connection)
             #ConventionImportHandler(self.connection)
-            AdministrationImportHandler(self.connection),
+            #AdministrationImportHandler(self.connection),
+            BailleurImportHandler(self.connection)
         ]
 
     def process(self):
