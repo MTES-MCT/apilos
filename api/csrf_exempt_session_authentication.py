22eafd3189eee2842365f7a5c47947ac0e6f14e5
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2022-10-24 15:41:42 +0200

    clean up old and unused API (#355)

--- api/csrf_exempt_session_authentication.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.authentication import SessionAuthentication
-
-# should be replaced by a Token session with an endpoint to get it.
-class CsrfExemptSessionAuthentication(SessionAuthentication):
-    def enforce_csrf(self, request):
-        return  # To not perform the csrf check previously happening
