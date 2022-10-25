85d3762673376d1026d66bc3a32026a0120e1f13
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2022-10-25 09:51:20 +0200

    Refactor handlers to ease dependencies

--- ecoloweb/services/handlers_administrations.py
+++ ecoloweb/services/handlers_administrations.py
@@ -4,21 +4,5 @@ from instructeurs.models import Administration
 
 
 class AdministrationImportHandler(ModelImportHandler):
-
-    def _get_sql_query(self, criteria: dict) -> str:
-        return self._get_sql_from_file('resources/sql/administrations.sql')
-
-    def _process_data(self, data: dict) -> bool:
-        ecolo_id = data.pop('id')
-
-        if ref := self._find_ecolo_reference(Administration, ecolo_id) is None:
-            administration, created = Administration.objects.get_or_create(code=data['code'], defaults=data)
-        else:
-            print(
-                f"Skipping administration with ecolo id #{ecolo_id}, already imported ({ref.apilos_model} #{ref.apilos_id})")
-            created = False
-
-        return created
-
-    def on_complete(self):
-        print(f"Migrated {self._count} administration(s)")
+    model = Administration
+    sql_template = 'resources/sql/administrations.sql'
