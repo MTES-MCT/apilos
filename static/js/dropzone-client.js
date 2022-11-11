6d3f98389fdf0f82fb73876d1d9c65a22d9234d4
Nicolas Oudard <nicolas@oudard.org>
2022-11-11 09:30:59 +0100

    missing let

--- static/js/dropzone-client.js
+++ static/js/dropzone-client.js
@@ -49,13 +49,12 @@ function init_dropzone_from_file(form_id, accepted_files) {
                 console.log('all_files_uploaded');
             });
             this.on("success", function (file, response) {
-                filename = encodeURI(file.name)
+                let filename = encodeURI(file.name)
                 if (document.querySelector('img[alt="'+filename+'"]') != null) {
                     file.thumbnail = document.querySelector('img[alt="'+filename+'"]').src;
                 }
                 for (var i = 0; i < response.uploaded_file.length; i++) {
-                    uploaded_filename = encodeURI(response.uploaded_file[i].filename)
-                    if ( uploaded_filename == filename) {
+                    if (filename == encodeURI(response.uploaded_file[i].filename)) {
                         file.uuid = response.uploaded_file[i].uuid
                     }
                 }
