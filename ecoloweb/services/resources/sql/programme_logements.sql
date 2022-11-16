e66ff1440f478b751711952441a4470418597473
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2022-11-16 15:19:20 +0100

    Fix unstable unnest + int casting

--- ecoloweb/services/resources/sql/programme_logements.sql
+++ ecoloweb/services/resources/sql/programme_logements.sql
@@ -3,18 +3,16 @@
 -- Champs restants à mapper:
 -- surface_annexes_retenue numeric(6, 2)
 -- loyer_par_metre_carre   numeric(6, 2)
--- bailleur_id  FK(bailleur)  not null
--- lot_id   FK(programmes_lot) not null
 select
     l.id,
     md5(pl.conventiondonneesgenerales_id||'-'||ff.code) as lot_id,
-    pl.conventiondonneesgenerales_id as programme_id,
-    l.numerobatiment||' '||l.noescalier||' '||l.etage||' '||l.numerologement as designation,
+    pl.bailleurproprietaire_id as bailleur_id,
+    coalesce(l.numerobatiment||' '||l.noescalier||' '||l.etage||' '||l.numerologement, '') as designation,
     ptl.code as typologie,
     l.surfacehabitable::float as surface_habitable,
     l.surfaceannexe::float as surface_annexes,
     l.surfaceutile::float as surface_utile,
-    l.coefficientmodulation as coefficient,
+    l.coefficientmodulation as coeficient,
     l.montantloyer as loyer,
     l.datecreation as cree_le,
     l.datecreation as mis_a_jour_le
--- ecoloweb/services/resources/sql/programme_reference_cadastrale.sql
+++ ecoloweb/services/resources/sql/programme_reference_cadastrale.sql
@@ -4,14 +4,13 @@
 -- cree_le       timestamp with time zone not null,
 -- mis_a_jour_le timestamp with time zone not null
 
-select
-    ic.id,
+select ic.id,
     pl.conventiondonneesgenerales_id as programme_id,
     pl.bailleurproprietaire_id as bailleur_id,
     ic.id,
     ic.section,
     -- le champs Parcelle d'Ecolo est "splitté" en autant de lignes qu'il y a de valeurs séparées par un caractère non alphanumérique
-    unnest(regexp_split_to_array(ic.parcelle, '[^0-9]+'))::int as numero,
+    unnest(regexp_split_to_array(ic.parcelle, '[^0-9]+')) as raw_numero,
     pl.description as lieudit,
     ic.superficie
 from ecolo.ecolo_infocadastrale ic
