2af1b31afdfb923f63be4e8f2799f273e824cce7
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2022-11-22 14:33:08 +0100

    Refacto cadastre (#398)
    * rename type bailleur to sous-nature bailleur
    
    * Refacto cadastre step (last one to refacto on named step)
    
    * with tests
    
    * Revert "rename type bailleur to sous-nature bailleur"
    
    This reverts commit 6bb1eb7289d8b4a03c6a9831c219e3ef2fc14f26.

---
+++ conventions/tests/views/test_cadastre_view.py
@@ -0,0 +1,56 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from conventions.tests.views.abstract import AbstractViewTestCase
+
+
+class ConventionCadastreViewTests(AbstractViewTestCase, TestCase):
+    def setUp(self):
+        super().setUp()
+        self.target_path = reverse(
+            "conventions:cadastre", args=[self.convention_75.uuid]
+        )
+        self.next_target_path = reverse(
+            "conventions:edd", args=[self.convention_75.uuid]
+        )
+        self.target_template = "conventions/cadastre.html"
+        self.error_payload = {
+            "permis_construire": "",
+            "form-TOTAL_FORMS": 2,
+            "form-INITIAL_FORMS": 0,
+            "form-0-uuid": "",
+            "form-0-section": "",
+            "form-0-numero": "13",
+            "form-0-lieudit": "Marseille",
+            "form-0-surface": "0 ha 1 a 27 ca",
+            "form-1-uuid": "",
+            "form-1-section": "AC",
+            "form-1-numero": "15",
+            "form-1-lieudit": "Marseille",
+            "form-1-surface": "0 ha 1 a 2 ca",
+            "effet_relatif_files": "{}",
+            "acte_de_propriete_files": "{}",
+            "certificat_adressage_files": "{}",
+        }
+        self.success_payload = {
+            "permis_construire": "",
+            "form-TOTAL_FORMS": 2,
+            "form-INITIAL_FORMS": 0,
+            "form-0-uuid": "",
+            "form-0-section": "AC",
+            "form-0-numero": "13",
+            "form-0-lieudit": "Marseille",
+            "form-0-surface": "0 ha 1 a 27 ca",
+            "form-1-uuid": "",
+            "form-1-section": "AC",
+            "form-1-numero": "15",
+            "form-1-lieudit": "Marseille",
+            "form-1-surface": "0 ha 1 a 2 ca",
+            "effet_relatif_files": "{}",
+            "acte_de_propriete_files": "{}",
+            "certificat_adressage_files": "{}",
+        }
+        self.msg_prefix = "[ConventionCadastreViewTests] "
+
+    def _test_data_integrity(self):
+        pass
