35d01b1f214f677f66a445c4f685a4db37cd7afc
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2023-01-18 10:42:25 +0100

    Integrate Crisp widget if configured

--- core/context_processor.py
+++ core/context_processor.py
@@ -6,6 +6,7 @@ from core import settings
 def get_environment(request):
     data = {}
     data["ENVIRONMENT"] = settings.ENVIRONMENT
+    data["CRISP_WEBSITE_ID"] = settings.CRISP_WEBSITE_ID
     data["CERBERE_AUTH"] = settings.CERBERE_AUTH
     data["CONVENTION_STATUT"] = {
         convention_statut.name: convention_statut.value
