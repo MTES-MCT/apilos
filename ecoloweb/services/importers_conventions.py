137092fe63f2fc20240ed8a935a45394c1903809
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2023-02-08 11:46:12 +0100

    Improve avenant / convention PDF / piece jointe management

--- ecoloweb/services/importers_conventions.py
+++ ecoloweb/services/importers_conventions.py
@@ -44,11 +44,12 @@ class ConventionImporter(ModelImporter):
         is_avenant = data.pop("is_avenant")
         parent_id = data.pop("parent_id")
         rank = data.pop("rank")
+        numero = data.pop("numero")
         return {
             "parent": self.import_one(parent_id if is_avenant else None),
             # For avenant conventions, "numero" is the rank of the iterations within the convention history,
             # minus 1 as first row is the root convention ranked 1
-            "numero": rank - 1 if is_avenant else data.pop("numero"),
+            "numero": (rank - 1) if is_avenant is True else numero,
             "lot": self._lot_importer.import_one(data.pop("lot_id")),
             "programme": self.resolve_ecolo_reference(
                 ecolo_id=data.pop("programme_id"), model=Programme
@@ -62,20 +63,30 @@ class ConventionImporter(ModelImporter):
         self._piece_jointe_importer.import_many(ecolo_id)
 
         if created and model is not None and not settings.TESTING:
+            piece_jointe = None
             if model.is_avenant():
                 # Avenant are automatically assigned to the type "commentaires"
                 model.avenant_types.add(AvenantType.objects.get(nom="commentaires"))
                 model.save()
 
-            piece_jointe = (
-                model.pieces_jointes.filter(
-                    type=PieceJointeType.AVENANT
-                    if model.is_avenant()
-                    else PieceJointeType.CONVENTION
+                # Le PDF assigné à un avenant est celui de la n-ième pièce jointe par ordre de création de type AVENANT,
+                # avec n le numéro de l'avenant
+                numero = int(model.numero)
+
+                if numero >= 1:
+                    try:
+                        piece_jointe = model.pieces_jointes.filter(
+                            type=PieceJointeType.AVENANT
+                        ).order_by("cree_le")[numero - 1]
+                    except IndexError:
+                        piece_jointe = None
+            else:
+                # Le PDF assigné à une convention est celui de la pièce jointe la plus récente de type CONVENTION
+                piece_jointe = (
+                    model.pieces_jointes.filter(type=PieceJointeType.CONVENTION)
+                    .order_by("-cree_le")
+                    .first()
                 )
-                .order_by("-cree_le")
-                .first()
-            )
 
             # Automatically promote the latest piece jointe with type CONVENTION as official convention document
             if piece_jointe is not None:
