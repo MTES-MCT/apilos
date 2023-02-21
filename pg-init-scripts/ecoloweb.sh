c43ae618cbfbf318cff013e4531b5ffe3918031d
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2023-02-21 20:43:36 +0100

    Add listen_address to conf & create ecolo db

--- pg-init-scripts/ecoloweb.sh
+++ pg-init-scripts/ecoloweb.sh
@@ -4,9 +4,10 @@ set -e
 set -u
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+create database ecoloweb;
 create database ecolotest;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" ecolotest <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname ecolotest <<-EOSQL
 create schema if not exists ecolo;
 EOSQL
