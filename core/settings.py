29b3b7c0f8afd42dc12dc461434b88f382e7d1fd
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2023-03-09 17:30:25 +0100

    add npm dependencies (#606)

--- core/settings.py
+++ core/settings.py
@@ -261,7 +261,12 @@ USE_TZ = True
 # Static files
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, "staticfiles"))
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    ("dsfr", BASE_DIR / "node_modules" / "@gouvfr" / "dsfr" / "dist"),
+    ("virtual-select", BASE_DIR / "node_modules" / "virtual-select-plugin" / "dist"),
+    ("turbo", BASE_DIR / "node_modules" / "@hotwired" / "turbo" / "dist"),
+]
 
 # Why STAGING = FALSE ?
 STAGING = False
