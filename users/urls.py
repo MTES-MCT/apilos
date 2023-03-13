7108dd0f322685df67d3d36b4c51f1ea2a0635ff
Pierre Lemée <pierre.lemee@beta.gouv.fr>
2023-03-13 09:00:43 +0100

    Amélioration du contexte de navigation lors de l'hijack (#592)
    * Improve filters is_bailleur & is_instucteur for better hijack usage
    
    * Remove useless currently change route for staff

--- users/urls.py
+++ users/urls.py
@@ -7,10 +7,5 @@ urlpatterns = [
         views.home,
         name="home",
     ),
-    path(
-        "update_currently",
-        views.update_currently,
-        name="update_currently",
-    ),
     path("read_popup", views.update_user_popup, name="read_popup"),
 ]
