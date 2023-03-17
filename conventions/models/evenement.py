3dac57d7cfbdede863260072b21298400753b7c0
Pierre LEMEE <pierre.lemee@beta.gouv.fr>
2023-03-17 14:53:07 +0100

    Add model & importer w/ SQL query

---
+++ conventions/models/evenement.py
@@ -0,0 +1,33 @@
+import uuid
+
+from django.utils import timezone
+from django.db import models
+
+from conventions.models import TypeEvenement
+
+
+class Evenement(models.Model):
+    id = models.AutoField(primary_key=True)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    convention = models.ForeignKey(
+        "Convention",
+        on_delete=models.CASCADE,
+        null=False,
+        related_name="evenements",
+    )
+    type_evenement = models.CharField(
+        max_length=50,
+        choices=TypeEvenement.choices,
+        default=TypeEvenement.DEPOT_BAILLEUR,
+    )
+    survenu_le = models.DateField(editable=False)
+    description = models.TextField(null=True, blank=True)
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        if not self.id and not self.survenu_le:
+            # Définition de la data d'évènement à la date actuelle si omise à la création
+            self.survenu_le = timezone.now()
+
+        super().save(force_insert, force_update, using, update_fields)
