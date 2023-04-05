6dbbf5195be7bd3713a4e4a67ec437583efc5c67
Nicolas Oudard <nicolas@oudard.org>
2023-04-05 08:14:02 +0200

    add a worker restart script and schedule it

---
+++ bin/restart_workers_script.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+if [ -n "$SCALINGO_API_TOKEN" ]; then
+    install-scalingo-cli
+    scalingo login --api-token $SCALINGO_API_TOKEN
+    scalingo --app apilos-staging restart worker
+else
+    echo "SCALINGO_API_TOKEN is not set"
+fi
