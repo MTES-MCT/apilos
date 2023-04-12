b5eb26d47875bada0ae75294c0372ae47dc8aabb
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2023-04-12 22:17:57 +0200

    return to the SIAP redirect to the right environment (#717)

--- core/context_processor.py
+++ core/context_processor.py
@@ -6,6 +6,7 @@ from core import settings
 def get_environment(request):
     data = {}
     data["ENVIRONMENT"] = settings.ENVIRONMENT
+    data["SIAP_CLIENT_HOST"] = settings.SIAP_CLIENT_HOST
     data["CRISP_WEBSITE_ID"] = settings.CRISP_WEBSITE_ID
     data["CERBERE_AUTH"] = settings.CERBERE_AUTH
     data["CONVENTION_STATUT"] = {
