f3968e3c2ad709d73920d4a82da705396136b203
Fabien Le Frapper <contact@fabienlefrapper.me>
2023-07-05 16:46:14 +0200

    Apply suggestions from code review

--- siap/siap_client/tests/test_siap_client_utils.py
+++ siap/siap_client/tests/test_siap_client_utils.py
@@ -133,7 +133,6 @@ class GetOrCreateProgrammeTest(TestCase):
 
 
 class GetOrCreateConventionFromOperationTest(TestCase):
-    op = {"detailsOperation": [{"aide": {"code": "COCO", "libelle": "PLS"}}]}
 
     def test_raises_when_unknown_aide(self):
         self.assertRaises(
@@ -150,9 +149,8 @@ class GetOrCreateConventionFromOperationTest(TestCase):
 
     def test_dont_raises_when_known_aide(self):
         # doesn't raise with NoConventionForOperationSIAPException
-        self.assertRaises(
-            KeyError,
-            utils.get_or_create_conventions,
+        try: 
+          utils.get_or_create_conventions(
             {
                 "detailsOperation": [
                     {"aide": {"code": "PLAI", "libelle": "PLAI"}},
@@ -160,4 +158,6 @@ class GetOrCreateConventionFromOperationTest(TestCase):
                 ]
             },
             User.objects.first(),
-        )
+          )
+        except Exception as exception:
+           self.assertNotEqual(exception, NoConventionForOperationSIAPException)
