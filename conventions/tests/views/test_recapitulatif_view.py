5d4e95fd212826370918b402dd141dfd6e19d54c
Fabien Le Frapper <contact@fabienlefrapper.me>
2023-07-28 15:12:13 +0200

    tests

--- conventions/tests/views/test_recapitulatif_view.py
+++ conventions/tests/views/test_recapitulatif_view.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from conventions.forms.convention_form_administration import (
+    UpdateConventionAdministrationForm,
+)
+from conventions.models.convention import Convention
 from conventions.tests.views.abstract import AbstractCreateViewTestCase
+from conventions.views.conventions import get_forms_for_convention
 
 
 class ConventionRecapitulatifTests(AbstractCreateViewTestCase, TestCase):
@@ -33,3 +38,50 @@ class ConventionRecapitulatifTests(AbstractCreateViewTestCase, TestCase):
             "update_programme_number": "1",
         }
         self.msg_prefix = "[ConventionRecapitulatifTests] "
+
+    def test_update_administration_redirect(self):
+        convention = Convention.objects.get(uuid="f590c593-e37f-4258-b38a-f8d2869969c4")
+        forms = get_forms_for_convention(convention)
+        assert "update_convention_administration_form" in forms
+
+        self.client.post(reverse("login"), {"username": "nicolas", "password": "12345"})
+        response = self.client.post(
+            f"/conventions/recapitulatif/{convention.uuid}",
+            data={
+                "convention": convention.pk,
+                "administration": "8b21f223-7121-4304-87ab-e2873cc1dcc1",
+                "verification": "transférer",
+            },
+        )
+
+        self.assertEqual(response["Location"], "/conventions/en-cours")
+        convention.refresh_from_db()
+        self.assertEqual(
+            str(convention.programme.administration.uuid),
+            "8b21f223-7121-4304-87ab-e2873cc1dcc1",
+        )
+
+    def test_update_administration_basic(self):
+        convention = Convention.objects.get(uuid="f590c593-e37f-4258-b38a-f8d2869969c4")
+        form = UpdateConventionAdministrationForm(
+            {
+                "convention": convention.pk,
+                "administration": "8b21f223-7121-4304-87ab-e2873cc1dcc1",
+                "verification": "transférer",
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_update_administration_verification(self):
+        convention = Convention.objects.get(uuid="f590c593-e37f-4258-b38a-f8d2869969c4")
+        form = UpdateConventionAdministrationForm(
+            {
+                "convention": convention.pk,
+                "administration": "8b21f223-7121-4304-87ab-e2873cc1dcc1",
+            }
+        )
+
+        self.assertEqual(
+            form.errors["verification"],
+            ["Vous devez recopier le mot pour valider l'opération"],
+        )
