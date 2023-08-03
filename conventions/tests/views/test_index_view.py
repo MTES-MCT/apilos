d38ace830a2e7ddee1e1672dce328ae7fb8f8484
Fabien Le Frapper <contact@fabienlefrapper.me>
2023-08-03 18:37:41 +0200

    test tabs

--- conventions/tests/views/test_index_view.py
+++ conventions/tests/views/test_index_view.py
@@ -1,7 +1,13 @@
 from bs4 import BeautifulSoup
-
+from django.http.request import HttpRequest
 from django.test import TestCase
 from django.urls import reverse
+
+from conventions.services.search import (
+    UserConventionSearchService,
+)
+from conventions.views.conventions import ConventionSearchView, ConventionTabsMixin
+from users.models import User
 
 
 class ConventionIndexViewTests(TestCase):
@@ -61,3 +67,30 @@ class ConventionIndexViewTests(TestCase):
 
         self.assertEqual({r.text.strip() for r in galion_refs}, {"12345", "98765"})
         self.assertEqual({f.text.strip() for f in financements}, {"PLUS", "PLAI"})
+
+    def test_get_tabs_basic(self):
+        class DummyClassWithTabs(ConventionTabsMixin):
+            def __init__(self):
+                self.request = HttpRequest()
+                self.request.user = User.objects.get(username="nicolas")
+
+        self.assertEqual(len(DummyClassWithTabs().get_tabs()), 3)
+
+    def test_get_tabs_with_new_view(self):
+        class DummyClassWithTabs(ConventionTabsMixin):
+            def __init__(self):
+                self.request = HttpRequest()
+                self.request.user = User.objects.get(username="nicolas")
+
+        class DummyService(UserConventionSearchService):
+            weight = 1000000
+            verbose_name = "Je suis un service"
+
+        # pylint: disable=W0611, unused-variable
+        class DummyConventionView(ConventionSearchView):
+            name = "search_instruction"
+            service_class = DummyService
+
+        tabs = DummyClassWithTabs().get_tabs()
+        self.assertEqual(len(tabs), 4)
+        self.assertEqual(tabs[-1]["title"], "Je suis un service")
