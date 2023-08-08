26ee64fa6559816946c9f4899c01b0631f64ddd9
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2023-08-09 00:19:55 +0200

    fix search in convention list (#914)

--- conventions/services/search.py
+++ conventions/services/search.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
 from collections import defaultdict
+from copy import copy
 from typing import List
 
 from django.conf import settings
 from django.core.paginator import Paginator
-from django.db.models import QuerySet
+from django.db.models import Q, QuerySet
 
 from bailleurs.models import Bailleur
 from conventions.models import Convention, ConventionStatut
@@ -18,6 +19,7 @@ class ConventionSearchBaseService(ABC):
     prefetch = []
     default_filters = defaultdict()
     filters: dict = {}
+    extra_filters = None
     statuses = []
 
     @property
@@ -29,6 +31,9 @@ class ConventionSearchBaseService(ABC):
         pass
 
     def _build_queryset_filters(self) -> None:
+        pass
+
+    def _build_queryset_extra_filters(self) -> None:
         pass
 
     def _get_order_by(self) -> List:
@@ -47,9 +52,13 @@ class ConventionSearchBaseService(ABC):
     def get_queryset(self) -> QuerySet:
         queryset = self._get_base_queryset()
         self._build_queryset_filters()
+        self._build_queryset_extra_filters()
 
         if self.filters:
             queryset = queryset.filter(**self.filters)
+
+        if self.extra_filters:
+            queryset = queryset.filter(self.extra_filters)
 
         if self.prefetch:
             queryset = queryset.prefetch_related(*self.prefetch)
@@ -97,7 +106,7 @@ class UserConventionSearchService(ConventionSearchBaseService):
     commune: str | None
     departement: str | None
     order_by: str | None
-    search_input: str | None
+    search_input: str | None = None
     commune: str | None = None
     financement: str | None = None
     statut: ConventionStatut | None = None
@@ -127,7 +136,7 @@ class UserConventionSearchService(ConventionSearchBaseService):
             self.statut = ConventionStatut.get_by_label(search_filters.get("statut"))
 
     def _build_queryset_filters(self):
-        self.filters = self.default_filters
+        self.filters = copy(self.default_filters)
 
         if self.statuses:
             self.filters["statut__in"] = map(lambda s: s.label, self.statuses)
@@ -146,6 +155,9 @@ class UserConventionSearchService(ConventionSearchBaseService):
 
         if self.administration:
             self.filters["lot__programme__administration__uuid"] = self.administration
+
+        if self.anru:
+            self.filters["lot__programme__anru"] = True
 
     def _get_base_queryset(self) -> QuerySet:
         return (
@@ -167,6 +179,14 @@ class UserConventionEnInstructionSearchService(UserConventionSearchService):
         ConventionStatut.A_SIGNER,
     ]
 
+    def _build_queryset_extra_filters(self):
+        if self.search_input:
+            self.extra_filters = (
+                Q(programme__nom__icontains=self.search_input)
+                | Q(programme__code_postal__icontains=self.search_input)
+                | Q(programme__numero_galion__icontains=self.search_input)
+            )
+
 
 class UserConventionActivesSearchService(UserConventionSearchService):
     weight = 10
@@ -174,6 +194,14 @@ class UserConventionActivesSearchService(UserConventionSearchService):
     verbose_name = "validée(s)"
     statuses = [ConventionStatut.SIGNEE]
     default_filters = {"parent_id": None}
+
+    def _build_queryset_extra_filters(self):
+        if self.search_input:
+            self.extra_filters = (
+                Q(programme__nom__icontains=self.search_input)
+                | Q(programme__code_postal__icontains=self.search_input)
+                | Q(numero__icontains=self.search_input)
+            )
 
 
 class UserConventionTermineesSearchService(UserConventionSearchService):
@@ -186,3 +214,11 @@ class UserConventionTermineesSearchService(UserConventionSearchService):
         ConventionStatut.ANNULEE,
     ]
     default_filters = {"parent_id": None}
+
+    def _build_queryset_extra_filters(self):
+        if self.search_input:
+            self.extra_filters = (
+                Q(programme__nom__icontains=self.search_input)
+                | Q(programme__code_postal__icontains=self.search_input)
+                | Q(numero__icontains=self.search_input)
+            )
