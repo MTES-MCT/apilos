28c3a78e7fb288b5d1e182ea57da93e0767c2744
Fabien Le Frapper <contact@fabienlefrapper.me>
2023-08-15 16:54:28 +0200

    Use temporary file

--- upload/tasks.py
+++ upload/tasks.py
@@ -1,9 +1,11 @@
 import logging
+import tempfile
 import subprocess
 from pathlib import Path
 
 from celery import shared_task
 from django.conf import settings
+from django.core.files.storage import default_storage
 
 from core.services import EmailService, EmailTemplateID
 from upload.models import UploadedFile
@@ -22,36 +24,44 @@ def scan_uploaded_files(paths_to_scan, authenticated_user_id):
     )
 
     for path, uploaded_file_id in paths_to_scan:
-        path = Path(settings.MEDIA_ROOT / path).resolve()
-        output = subprocess.run(
-            f'clamdscan {path} --config-file="{settings.CLAMAV_PATH}/clamav/clamd.conf"',
-            capture_output=True,
-            shell=True,
-            check=True,
-            text=True,
-        )
+        file_path = Path(settings.MEDIA_ROOT / path).resolve()
 
-        if (
-            "Infected files" in output.stdout
-            and "Infected files: 0" not in output.stdout
-        ):
-            user = User.objects.get(id=authenticated_user_id)
-            EmailService(
-                to_emails=[user.email],
-                email_template_id=EmailTemplateID.VIRUS_WARNING,
-            ).send_transactional_email(
-                email_data={
-                    "firstname": user.first_name,
-                    "lastname": user.last_name,
-                    "filename": path.name,
-                }
-            )
-            path.unlink()
-            UploadedFile.objects.get(id=uploaded_file_id).delete()
+        with default_storage.open(file_path, "rb") as original_file_object:
+            file_is_infected = False
 
-            logger.warning(
-                "An infected file has been detected."
-                "The user has been warned by email "
-                "and the file has succesfully been removed. | File location : %s",
-                path,
-            )
+            with tempfile.NamedTemporaryFile() as tf:
+                tf.write(original_file_object.read())
+                output = subprocess.run(
+                    f'clamdscan {tf.name} --config-file="{settings.CLAMAV_PATH}/clamav/clamd.conf"',
+                    capture_output=True,
+                    shell=True,
+                    check=False,
+                    text=True,
+                )
+
+                file_is_infected = (
+                    "Infected files" in output.stdout
+                    and "Infected files: 0" not in output.stdout
+                )
+            if file_is_infected:
+                user = User.objects.get(id=authenticated_user_id)
+                EmailService(
+                    to_emails=[user.email],
+                    email_template_id=EmailTemplateID.VIRUS_WARNING,
+                ).send_transactional_email(
+                    email_data={
+                        "firstname": user.first_name,
+                        "lastname": user.last_name,
+                        "filename": path.name,
+                    }
+                )
+
+                default_storage.delete(file_path)
+                UploadedFile.objects.get(id=uploaded_file_id).delete()
+
+                logger.warning(
+                    "An infected file has been detected."
+                    "The user has been warned by email "
+                    "and the file has succesfully been removed. | File location : %s",
+                    path,
+                )
