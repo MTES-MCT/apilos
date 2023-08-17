362abe85861b4f71add3b0926dd4a33e28a65614
Fabien Le Frapper <contact@fabienlefrapper.me>
2023-08-17 14:26:55 +0200

    Disable django cas ng mesages

--- core/settings.py
+++ core/settings.py
@@ -509,6 +509,7 @@ if CERBERE_AUTH:
         "UTILISATEUR.PRENOM": "first_name",
         "UTILISATEUR.MEL": "email",
     }
+    CAS_LOGIN_MSG = None
     CAS_LOGGED_MSG = None
 
     LOGIN_URL = "/accounts/cerbere-login"
