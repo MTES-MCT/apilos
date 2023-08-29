a752d49b646e47ca66a6e8145b08d0eb32384ed3
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2023-08-29 13:21:12 +0200

    manage min and max value to end date of conventionnement (#942)

--- conventions/tests/services/test_financement_service.py
+++ conventions/tests/services/test_financement_service.py
@@ -1,18 +1,35 @@
+import datetime
+
 from django.http import HttpRequest
 from django.test import TestCase
 
-from conventions.forms import (
-    ConventionFinancementForm,
-    PretFormSet,
-    UploadForm,
-)
+from conventions.forms import ConventionFinancementForm, PretFormSet, UploadForm
 from conventions.models import Convention, Pret, Preteur
-from conventions.services import (
-    financement as financement_service,
-    utils,
-)
+from conventions.services import financement as financement_service
+from conventions.services import utils
 from programmes.models import TypeOperation
 from users.models import User
+
+financement_form = {
+    "form-TOTAL_FORMS": 2,
+    "form-INITIAL_FORMS": 2,
+    "form-0-uuid": "",
+    "form-0-numero": "A",
+    "form-0-date_octroi": "2020-01-01",
+    "form-0-duree": "50",
+    "form-0-montant": "1000000.00",
+    "form-0-preteur": "CDCF",
+    "form-0-autre": "",
+    "form-1-uuid": "",
+    "form-1-numero": "A",
+    "form-1-date_octroi": "2020-01-01",
+    "form-1-duree": "20",
+    "form-1-montant": "200000.00",
+    "form-1-preteur": "CDCL",
+    "form-1-autre": "",
+    "annee_fin_conventionnement": "2093",
+    "fond_propre": "",
+}
 
 
 class ConventionFinancementServiceTests(TestCase):
@@ -41,52 +58,8 @@ class ConventionFinancementServiceTests(TestCase):
         self.assertIsInstance(self.service.form, ConventionFinancementForm)
         self.assertIsInstance(self.service.upform, UploadForm)
 
-    def test_save(self):
-        self.service.request.POST = {
-            "form-TOTAL_FORMS": 2,
-            "form-INITIAL_FORMS": 2,
-            "form-0-uuid": "",
-            "form-0-numero": "A",
-            "form-0-date_octroi": "2020-01-01",
-            "form-0-duree": "50",
-            "form-0-montant": "1000000.00",
-            "form-0-preteur": "CDCF",
-            "form-0-autre": "",
-            "form-1-uuid": "",
-            "form-1-numero": "A",
-            "form-1-date_octroi": "2020-01-01",
-            "form-1-duree": "",
-            "form-1-montant": "200000.00",
-            "form-1-preteur": "CDCL",
-            "form-1-autre": "",
-            "annee_fin_conventionnement": "2093",
-            "fond_propre": "",
-        }
-        self.service.save()
-        self.assertEqual(self.service.return_status, utils.ReturnStatus.ERROR)
-        form = self.service.formset.forms[1]
-        self.assertTrue(form.has_error("duree"))
-
-        self.service.request.POST = {
-            "form-TOTAL_FORMS": 2,
-            "form-INITIAL_FORMS": 2,
-            "form-0-uuid": "",
-            "form-0-numero": "A",
-            "form-0-date_octroi": "2020-01-01",
-            "form-0-duree": "50",
-            "form-0-montant": "1000000.00",
-            "form-0-preteur": "CDCF",
-            "form-0-autre": "",
-            "form-1-uuid": "",
-            "form-1-numero": "A",
-            "form-1-date_octroi": "2020-01-01",
-            "form-1-duree": "20",
-            "form-1-montant": "200000.00",
-            "form-1-preteur": "CDCL",
-            "form-1-autre": "",
-            "annee_fin_conventionnement": "2093",
-            "fond_propre": "",
-        }
+    def test_save_success(self):
+        self.service.request.POST = financement_form
 
         self.service.save()
         self.assertEqual(self.service.return_status, utils.ReturnStatus.SUCCESS)
@@ -101,61 +74,60 @@ class ConventionFinancementServiceTests(TestCase):
         self.assertEqual(pret_cdcl.montant, 200000.00)
         self.assertEqual(pret_cdcl.duree, 20)
 
-    def test_cdc_is_needed(self):
+    def test_duree_is_needed(self):
+        self.service.request.POST = {**financement_form, "form-1-duree": ""}
+        self.service.save()
+        self.assertEqual(self.service.return_status, utils.ReturnStatus.ERROR)
+        form = self.service.formset.forms[1]
+        self.assertTrue(form.has_error("duree"))
+
+    def test_cdc_is_needed_failed(self):
         programme = self.service.convention.programme
         programme.type_operation = TypeOperation.NEUF
         programme.save()
 
         self.service.request.POST = {
-            "form-TOTAL_FORMS": 2,
-            "form-INITIAL_FORMS": 2,
-            "form-0-uuid": "",
-            "form-0-numero": "A",
-            "form-0-date_octroi": "2020-01-01",
-            "form-0-duree": "50",
-            "form-0-montant": "1000000.00",
+            **financement_form,
             "form-0-preteur": Preteur.ETAT,
-            "form-0-autre": "",
-            "form-1-uuid": "",
-            "form-1-numero": "A",
-            "form-1-date_octroi": "2020-01-01",
-            "form-1-duree": "20",
-            "form-1-montant": "200000.00",
             "form-1-preteur": Preteur.COMMUNE,
-            "form-1-autre": "",
-            "annee_fin_conventionnement": "2093",
-            "fond_propre": "",
         }
         self.service.save()
         self.assertEqual(self.service.return_status, utils.ReturnStatus.ERROR)
         self.assertTrue(self.service.formset.non_form_errors())
 
-        programme.type_operation = TypeOperation.SANSTRAVAUX
-        programme.save()
-        self.service.save()
-        self.assertFalse(self.service.formset.non_form_errors())
-
+    def test_cdc_is_needed_success(self):
+        programme = self.service.convention.programme
         programme.type_operation = TypeOperation.NEUF
         programme.save()
         self.service.request.POST = {
-            "form-TOTAL_FORMS": 2,
-            "form-INITIAL_FORMS": 2,
-            "form-0-uuid": "",
-            "form-0-numero": "A",
-            "form-0-date_octroi": "2020-01-01",
-            "form-0-duree": "50",
-            "form-0-montant": "1000000.00",
+            **financement_form,
             "form-0-preteur": Preteur.CDCF,
-            "form-0-autre": "",
-            "form-1-uuid": "",
-            "form-1-numero": "A",
-            "form-1-date_octroi": "2020-01-01",
-            "form-1-duree": "20",
-            "form-1-montant": "200000.00",
             "form-1-preteur": Preteur.COMMUNE,
-            "form-1-autre": "",
-            "annee_fin_conventionnement": "2093",
-            "fond_propre": "",
         }
         self.service.save()
         self.assertFalse(self.service.formset.non_form_errors())
+
+    def test_cdc_isnot_needed_success(self):
+        programme = self.service.convention.programme
+        programme.type_operation = TypeOperation.SANSTRAVAUX
+        programme.save()
+        self.service.request.POST = {
+            **financement_form,
+            "form-0-preteur": Preteur.ETAT,
+            "form-1-preteur": Preteur.COMMUNE,
+        }
+        self.service.save()
+        self.assertFalse(self.service.formset.non_form_errors())
+
+    def test_year_isnot_too_far(self):
+        programme = self.service.convention.programme
+        programme.type_operation = TypeOperation.NEUF
+        programme.save()
+        self.service.request.POST = {
+            **financement_form,
+            "annee_fin_conventionnement": datetime.date.today().year + 501,
+        }
+        self.service.save()
+
+        form = self.service.form
+        self.assertTrue(form.has_error("annee_fin_conventionnement"))
