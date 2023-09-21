f589ee31535c6f3286414a091f246dda35103ac9
Marjolaine Le Bon <marjolaine@happy-dev.fr>
2023-09-21 11:19:11 +0200

    fix erreur à la correction

--- conventions/services/denonciation.py
+++ conventions/services/denonciation.py
@@ -28,22 +28,39 @@ class ConventionDenonciationService(ConventionService):
 
     def save(self):
         self.request.user.check_perm("convention.change_convention", self.convention)
-        initial = {
-            "date_denonciation": utils.format_date_for_form(
-                self.convention.date_denonciation
-            ),
-        }
-        self.form = ConventionDenonciationForm(self.request.POST, initial=initial)
+        self.redirect_recap = bool(self.request.POST.get("redirect_to_recap", False))
+        self._denonciation_atomic_update()
+
+    def _denonciation_atomic_update(self):
+        self.form = ConventionDenonciationForm(
+            {
+                "uuid": self.convention.uuid,
+                **utils.build_partial_form(
+                    self.request,
+                    self.convention,
+                    [
+                        "date_denonciation",
+                        "motif_denonciation",
+                    ],
+                ),
+                **utils.init_text_and_files_from_field(
+                    self.request,
+                    self.convention,
+                    "fichier_denonciation",
+                ),
+            }
+        )
         if self.form.is_valid():
-            self.convention.date_denonciation = self.form.cleaned_data[
-                "date_denonciation"
-            ]
-            self.convention.motif_denonciation = self.form.cleaned_data[
-                "motif_denonciation"
-            ]
-            self.convention.fichier_denonciation = utils.set_files_and_text_field(
-                self.form.cleaned_data["fichier_denonciation_files"],
-                self.form.cleaned_data["fichier_denonciation"],
-            )
-            self.convention.save()
+            self._save_denonciation()
             self.return_status = utils.ReturnStatus.SUCCESS
+
+    def _save_denonciation(self):
+        self.convention.date_denonciation = self.form.cleaned_data["date_denonciation"]
+        self.convention.motif_denonciation = self.form.cleaned_data[
+            "motif_denonciation"
+        ]
+        self.convention.fichier_denonciation = utils.set_files_and_text_field(
+            self.form.cleaned_data["fichier_denonciation_files"],
+            self.form.cleaned_data["fichier_denonciation"],
+        )
+        self.convention.save()
