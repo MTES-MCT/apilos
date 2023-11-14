3709987b2f0aeae540b8dbfb2bbad24c4ba4ceca
Marjolaine Le Bon <marjolaine@happy-dev.fr>
2023-11-14 10:16:15 +0100

    fix: ajout du avenant_denonciation_step dans la view Dénonciation

--- conventions/views/convention_form_denonciation.py
+++ conventions/views/convention_form_denonciation.py
@@ -1,11 +1,12 @@
 from conventions.models import Convention
 from conventions.services.denonciation import ConventionDenonciationService
-from conventions.views.convention_form import ConventionView
+from conventions.views.convention_form import ConventionView, avenant_denonciation_step
 
 
 class DenonciationView(ConventionView):
     target_template: str = "conventions/denonciation.html"
     service_class = ConventionDenonciationService
+    form_steps = [avenant_denonciation_step]
 
     def _get_convention(self, convention_uuid):
         return Convention.objects.get(uuid=convention_uuid)
