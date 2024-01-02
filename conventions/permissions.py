13733e879f68fd3a7f6fec19ca28274f35d7b1f8
Etchegoyen Matthieu <etchegom@gmail.com>
2024-01-02 17:56:25 +0100

    fixing tests

--- conventions/permissions.py
+++ conventions/permissions.py
@@ -1,6 +1,7 @@
 # Decorator that check the user has permission and scope to handle action
 # for a given convention
 from conventions.models.convention import Convention
+from django.core.exceptions import PermissionDenied
 
 
 def has_campaign_permission(permission):
@@ -17,7 +18,10 @@ def has_campaign_permission(permission):
 def has_campaign_permission_view_function(permission):
     def has_permission(function):
         def wrapper(request, convention_uuid, **kwargs):
-            convention = Convention.objects.get(uuid=convention_uuid)
+            try:
+                convention = Convention.objects.get(uuid=convention_uuid)
+            except Convention.DoesNotExist:
+                raise PermissionDenied
             request.user.check_perm(permission, convention)
             return function(request, convention_uuid, **kwargs)
 
--- conventions/tests/views/test_conventions.py
+++ conventions/tests/views/test_conventions.py
@@ -15,16 +15,12 @@ class ConventionViewsTests(TestCase):
     ]
 
     def test_delete_convention_unknown_uuid(self):
-        response = self.client.post(
-            reverse("login"), {"username": "nicolas", "password": "12345"}
-        )
+        self.client.login(username="nicolas", password="12345")
+
         delete_path = reverse(
             "conventions:delete",
             kwargs={"convention_uuid": "b3d66d4e-0841-41a4-b643-4dcc37147391"},
         )
-        index_path = reverse("conventions:index")
 
         response = self.client.post(delete_path)
-
-        assert response.status_code == 302
-        self.assertRedirects(response, index_path)
+        assert response.status_code == 403
