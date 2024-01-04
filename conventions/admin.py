ac04eb23baee7eea17311d2bd8b024159383afd3
Etchegoyen Matthieu <etchegom@gmail.com>
2024-01-04 20:25:41 +0100

    correction de la sauvegarde du champ statut de la convention depuis la page d'admin

--- conventions/admin.py
+++ conventions/admin.py
@@ -1,11 +1,13 @@
 from typing import Any
 
+from django import forms
 from django.contrib import admin
 from django.contrib.admin import SimpleListFilter
 from django.db.models import QuerySet
 from django.http import HttpRequest
 
 from admin.admin import ApilosModelAdmin
+from conventions.models.choices import ConventionStatut
 
 from .models import AvenantType, Convention, Pret
 
@@ -39,6 +41,30 @@ class IsAvenantFilter(SimpleListFilter):
                 return queryset.filter(parent__isnull=True)
             case _:
                 return queryset
+
+
+class ConventionModelForm(forms.ModelForm):
+    statut = forms.ChoiceField(choices=ConventionStatut.choices)
+
+    def __init__(self, *args, **kwargs):
+        initial = kwargs.get("initial", {})
+
+        instance = kwargs.get("instance", None)
+        if instance:
+            statut = ConventionStatut.get_by_label(instance.statut)
+            if statut:
+                initial["statut"] = statut.name
+
+        super().__init__(initial=initial, *args, **kwargs)
+
+    def _post_clean(self):
+        super()._post_clean()
+        statut = self.cleaned_data.get("statut")
+        self.cleaned_data["statut"] = ConventionStatut[statut].label
+
+    class Meta:
+        model = Convention
+        exclude = []
 
 
 @admin.register(Convention)
@@ -100,6 +126,8 @@ class ConventionAdmin(ApilosModelAdmin):
         IsAvenantFilter,
         "cree_le",
     )
+
+    form = ConventionModelForm
 
 
 @admin.register(Pret)
