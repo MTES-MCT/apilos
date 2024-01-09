d0d402bed395f0fb5ea1bf1779c560ce77e813d2
Sylvain Delabye <syl.delabye@gmail.com>
2024-01-09 14:54:12 +0100

    Add admin command to reassign conventions administration

---
+++ conventions/management/commands/reassign_administration.py
@@ -0,0 +1,99 @@
+import logging
+from datetime import datetime
+
+from django.core.management.base import BaseCommand, CommandParser
+
+from conventions.models import Convention
+from instructeurs.models import Administration
+from programmes.models import Programme
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            "--start-date",
+            required=True,
+            help="Creation date YYYY-MM-DD from which to reassign conventions",
+            action="store",
+            nargs=1,
+        )
+        parser.add_argument(
+            "--end-date",
+            required=True,
+            help="Creation date YYYY-MM-DD until which to reassign conventions",
+            action="store",
+            nargs=1,
+        )
+        parser.add_argument(
+            "--current-admin-code",
+            required=True,
+            help="Administration currently assigned to the conventions (code)",
+            action="store",
+            nargs=1,
+        )
+        parser.add_argument(
+            "--new-admin-code",
+            required=True,
+            help="New administration to assign to the conventions (code)",
+            action="store",
+            nargs=1,
+        )
+        parser.add_argument(
+            "--dry-run", type=bool, help="Dry run mode", nargs="?", default=True
+        )
+        return super().add_arguments(parser)
+
+    def handle(self, *args, **kwargs):
+        print("Reassign administration command called")
+
+        # Parse arguments
+        start_date = datetime.strptime(kwargs["start_date"], "%Y-%m-%d").date()
+        end_date = datetime.strptime(kwargs["end_date"], "%Y-%m-%d").date()
+        current_admin = Administration.objects.get(code=kwargs["current_admin_code"])
+        new_admin = Administration.objects.get(code=kwargs["new_admin_code"])
+        dry_run = kwargs["dry_run"]
+
+        # Find the conventions assigned to current_admin
+        programmes = Programme.objects.filter(administration=current_admin)
+        conventions = Convention.objects.filter(programme__in=programmes)
+
+        # Filter the conventions to be in the date range
+        conventions = conventions.filter(cree_le__range=[start_date, end_date])
+
+        # Get programs to udpate
+        programmes_filtered = Programme.objects.filter(
+            id__in=conventions.values_list("programme_id")
+        )
+        print(f"Found {programmes_filtered.count()} programmes to update")
+
+        # Get the list of conventions not in range but attached to thesse programmes to warn user
+        conventions_also_impacted = Convention.objects.filter(
+            programme__in=programmes_filtered
+        ).exclude(id__in=conventions.values_list("id"))
+
+        print("Summary before execution:")
+        print("--------------------------")
+        print(f"{programmes_filtered.count()} programmes will be updated:")
+        for p in programmes_filtered:
+            print(f"    - {p.nom} - {p.numero_galion}")
+        print("--------------------------")
+        print(f"{conventions.count()} conventions in the date range will be impacted:")
+        for c in conventions:
+            print("    - " + str(c))
+        print("--------------------------")
+        if conventions_also_impacted.count() > 0:
+            print(
+                f"WARNING {conventions_also_impacted.count()} conventions out of "
+                f"the date range will be impacted as a side-effect:"
+            )
+            for c in conventions_also_impacted:
+                print("    - " + str(c))
+
+        if dry_run:
+            print("Dry run mode, no changes executed")
+        else:
+            # Update administration
+            programmes_filtered.update(administration=new_admin)
+            print("Changes executed")
---
+++ conventions/tests/commands/test_reassign_administration.py
@@ -0,0 +1,114 @@
+from datetime import datetime, timezone
+
+from django.core.management import call_command
+from django.test import TestCase
+
+from conventions.models import Convention
+from instructeurs.models import Administration
+from programmes.models import Programme
+
+
+class ReassignAdministrationTest(TestCase):
+    fixtures = [
+        "auth.json",
+        "avenant_types.json",
+        "bailleurs_for_tests.json",
+        "instructeurs_for_tests.json",
+        "programmes_for_tests.json",
+        "conventions_for_tests.json",
+        "users_for_tests.json",
+    ]
+
+    def setUp(self):
+        self.admin1 = Administration.objects.create(code="3311")
+        self.admin2 = Administration.objects.create(code="3312")
+        self.admin3 = Administration.objects.create(code="3313")
+
+        self.programme1 = Programme.objects.create(
+            administration=self.admin1, bailleur_id=1, nom="Programme 1"
+        )
+        self.programme2 = Programme.objects.create(
+            administration=self.admin1, bailleur_id=1, nom="Programme 2"
+        )
+        self.programme3 = Programme.objects.create(
+            administration=self.admin2, bailleur_id=1, nom="Programme 3"
+        )
+
+        self.convention1 = Convention.objects.create(
+            programme=self.programme1, lot_id=1, numero="991"
+        )
+        self.convention2 = Convention.objects.create(
+            programme=self.programme2, lot_id=2, numero="992"
+        )
+        self.convention3 = Convention.objects.create(
+            programme=self.programme3, lot_id=3, numero="993"
+        )
+        self.convention2_1 = Convention.objects.create(
+            programme=self.programme2, lot_id=1, numero="9921"
+        )
+
+        self.convention1.cree_le = datetime(
+            year=2019, month=1, day=1, tzinfo=timezone.utc
+        )
+        self.convention2.cree_le = datetime(
+            year=2020, month=12, day=31, tzinfo=timezone.utc
+        )
+        self.convention3.cree_le = datetime(
+            year=2020, month=1, day=1, tzinfo=timezone.utc
+        )
+        self.convention2_1.cree_le = datetime(
+            year=2028, month=2, day=20, tzinfo=timezone.utc
+        )
+        self.convention1.save()
+        self.convention2.save()
+        self.convention3.save()
+
+    def _refresh_from_db(self):
+        self.programme1.refresh_from_db()
+        self.programme2.refresh_from_db()
+        self.programme3.refresh_from_db()
+
+        self.convention1.refresh_from_db()
+        self.convention2.refresh_from_db()
+        self.convention3.refresh_from_db()
+
+    def test_reassign_administration_different_programs(self):
+        args = []
+        kwargs = {
+            "start_date": "2020-01-01",
+            "end_date": "2021-12-31",
+            "current_admin_code": str(self.admin1.code),
+            "new_admin_code": str(self.admin3.code),
+            "dry_run": False,
+        }
+        call_command("reassign_administration", *args, **kwargs)
+
+        self._refresh_from_db()
+
+        # Out of range: no change
+        assert self.convention1.administration == self.admin1
+        # In range: assigned to new admin
+        assert self.convention2.administration == self.admin3
+        # In range but assigned to other admin: no change
+        assert self.convention3.administration == self.admin2
+        # Out of range but using the same program as
+        # a convention that has been reassigned: new admin
+        assert self.convention2_1.administration == self.admin3
+
+    def test_reassign_administration_different_programs_dry_run(self):
+        args = []
+        kwargs = {
+            "start_date": "2020-01-01",
+            "end_date": "2021-12-31",
+            "current_admin_code": str(self.admin1.code),
+            "new_admin_code": str(self.admin3.code),
+        }
+        call_command("reassign_administration", *args, **kwargs)
+
+        self._refresh_from_db()
+
+        # Dry run: no changes
+        assert self.convention1.administration == self.admin1
+        assert self.convention2.administration == self.admin1
+        assert self.convention3.administration == self.admin2
+        assert self.convention2_1.administration == self.admin1
