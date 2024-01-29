b5548ad889f9db81a322fedf4d19d42c73b0f190
Nicolas Oudard <nicolas@oudard.org>
2024-01-29 12:11:04 +0100

    Revert "script to remove orphan programmes and lots"
    This reverts commit 3a0bc8447725650647b7eff1ef8e42e00d6e18f1.

--- conventions/management/commands/remove_programme_without_convention.py
+++ conventions/management/commands/remove_programme_without_convention.py
@@ -5,39 +5,7 @@ from programmes.models import Programme
 
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        parser.add_argument(
-            "--dry-run",
-            help="Dry run",
-            action="store_true",
-        )
-
     def handle(self, *args, **options):
-        dry_run = options.get("dry_run")
-
-        programmes = (
-            Programme.objects.all()
-            .annotate(convention_count=Count("conventions"))
-            .filter(convention_count=0)
-        )
-
-        self.stdout.write(
-            self.style.SUCCESS(
-                f"Found {programmes.count()} programmes without conventions to be removed: "
-            )
-        )
-        for programme in programmes:
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f" - {str(programme.uuid)} - {programme.nom} - {programme.numero_galion} - {programme.cree_le}"
-                )
-            )
-
-        if not dry_run:
-            for programme in programmes:
-                programme.delete()
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f"Removed {programmes.count()} programmes without conventions"
-                )
-            )
+        Programme.objects.all().annotate(convention_count=Count("conventions")).filter(
+            convention_count=0
+        ).delete()
