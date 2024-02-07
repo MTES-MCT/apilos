a7c98566dd909635e9b57b77dfbf1509cfe83fb6
Etchegoyen Matthieu <etchegom@gmail.com>
2024-02-07 10:33:23 +0100

    simplifie l'ajout d'utilisateurs à un feature flag waffle depuis l'admin

--- apilos_settings/admin.py
+++ apilos_settings/admin.py
@@ -12,9 +12,33 @@ class WaffleCustomAdminMixin(ApilosModelAdmin):
     staff_user_can_delete = True
 
 
+class FlagUserInline(admin.TabularInline):
+    model = Flag.users.through
+    extra = 1
+    verbose_name_plural = "Activer ce flag pour ces utilisateurs"
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "user":
+            kwargs["queryset"] = self.get_field_queryset(
+                kwargs.get("using"), db_field, request
+            ).order_by("last_name")
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
 @admin.register(Flag)
 class CustomFlagAdmin(WaffleCustomAdminMixin, FlagAdmin):
-    pass
+    inlines = (FlagUserInline,)
+    fields = (
+        "name",
+        "everyone",
+        "superusers",
+        "staff",
+        "authenticated",
+        "groups",
+        "note",
+        "users",
+    )
+    readonly_fields = ("name",)
 
 
 @admin.register(Sample)
