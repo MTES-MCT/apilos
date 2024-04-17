bbf741b468878fd39edd48a9a57c5dbed38cd23a
Nicolas Oudard <nicolas@oudard.org>
2024-04-17 16:12:47 +0200

    merge migration for ecolo, comments and upload

--- scripts/import-csv.py
+++
@@ -1,6 +0,0 @@
-# ce script ne marche pas : incompatibilité de openpyyxl et csv
-
-from openpyxl import load_workbook
-
-wb2 = load_workbook("../documents/export_sisal.csv")
-print(wb2.sheetnames)  # noqa: T201
