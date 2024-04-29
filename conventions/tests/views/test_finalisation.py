a9978c8073af30ff42856928bd25bd89dfecb2cf
Sylvain Delabye <syl.delabye@gmail.com>
2024-04-29 11:29:54 +0200

    Override cerfa avenants (#1385)
    * Entrée dans le parcours de finalisation d'avenant
    
    * Adapt override for avenants
    
    * Add tests
    
    * Remove init override

--- conventions/tests/views/test_finalisation.py
+++ conventions/tests/views/test_finalisation.py
@@ -4,6 +4,10 @@ from django.urls import reverse
 from pytest_django.asserts import assertTemplateUsed
 from waffle.testutils import override_flag
 
+from conventions.forms.convention_form_finalisation import (
+    FinalisationCerfaForm,
+    FinalisationNumeroForm,
+)
 from conventions.models.choices import ConventionStatut
 from conventions.tests.factories import ConventionFactory
 
@@ -18,6 +22,17 @@ def convention():
     )
 
 
+@pytest.fixture
+def avenant(convention):
+    return ConventionFactory(
+        statut=ConventionStatut.INSTRUCTION.label,
+        numero="3456",
+        parent_id=convention.id,
+        fichier_override_cerfa='{"files": {"705e2175-4a1d-4670-b9d0-de8f3c4a5432": {"uuid": "705e2175-4a1d-4670-b9d0-'
+        'de8f3c4a5432", "size": 345913, "filename": "Cerfa.docx"}}, "text": ""}',
+    )
+
+
 @pytest.mark.django_db
 @override_flag(settings.FLAG_OVERRIDE_CERFA, active=True)
 def test_get_numero(client, convention):
@@ -26,6 +41,26 @@ def test_get_numero(client, convention):
     assert response.status_code == 200
     assertTemplateUsed(response, "conventions/finalisation/numero.html")
     assert "1234" in str(response.content)
+    assert (
+        response.context["form_step"]["current_step"]
+        == "Valider le numéro de la convention"
+    )
+    assert isinstance(response.context["form"], FinalisationNumeroForm)
+
+
+@pytest.mark.django_db
+@override_flag(settings.FLAG_OVERRIDE_CERFA, active=True)
+def test_get_numero_avenant(client, avenant):
+    url = reverse("conventions:finalisation_numero", args=[avenant.uuid])
+    response = client.get(url)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "conventions/finalisation/numero.html")
+    assert "3456" in str(response.content)
+    assert (
+        response.context["form_step"]["current_step"]
+        == "Valider le numéro de l'avenant"
+    )
+    assert isinstance(response.context["form"], FinalisationNumeroForm)
 
 
 @pytest.mark.django_db
@@ -60,6 +95,8 @@ def test_get_cerfa(client, convention):
     response = client.get(url)
     assert response.status_code == 200
     assertTemplateUsed(response, "conventions/finalisation/cerfa.html")
+    assert response.context["form_step"]["current_step"] == "Vérifier le document CERFA"
+    assert isinstance(response.context["form"], FinalisationCerfaForm)
 
 
 @pytest.mark.django_db
@@ -83,3 +120,20 @@ def test_get_validation(client, convention):
     response = client.get(url)
     assert response.status_code == 200
     assertTemplateUsed(response, "conventions/finalisation/validation.html")
+    assert (
+        response.context["form_step"]["current_step"]
+        == "Valider et envoyer la convention pour signature"
+    )
+
+
+@pytest.mark.django_db
+@override_flag(settings.FLAG_OVERRIDE_CERFA, active=True)
+def test_get_validation_avenant(client, avenant):
+    url = reverse("conventions:finalisation_validation", args=[avenant.uuid])
+    response = client.get(url)
+    assert response.status_code == 200
+    assertTemplateUsed(response, "conventions/finalisation/validation.html")
+    assert (
+        response.context["form_step"]["current_step"]
+        == "Valider et envoyer l'avenant pour signature"
+    )
