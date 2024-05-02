0f8506d95bf5972a942bf3d1ecd623521ff21610
Etchegoyen Matthieu <etchegom@gmail.com>
2024-05-02 11:31:14 +0200

    get rid of dead code

--- conventions/tests/custom_filters/test_convention_list_custom_filters.py
+++ conventions/tests/custom_filters/test_convention_list_custom_filters.py
@@ -4,7 +4,6 @@ from django.contrib.sessions.middleware import SessionMiddleware
 from django.test import RequestFactory, TestCase
 
 from conventions.templatetags import custom_filters
-from users.models import GroupProfile
 
 
 class TestGetOrderedFullPathFilter(TestCase):
@@ -39,96 +38,4 @@ class TestGetOrderedFullPathFilter(TestCase):
         self.assertEqual(
             custom_filters.get_ordered_full_path(self.request, "field"),
             "/conventions/en-cours?order_by=-field&page=1",
-        )
-
-
-class TestGetAvailableOrderFieldsFilter(TestCase):
-    def setUp(self):
-        get_response = mock.MagicMock()
-        self.request = RequestFactory().get("/conventions")
-        middleware = SessionMiddleware(get_response)
-        middleware.process_request(self.request)
-        self.request.session.save()
-
-    def test_get_ordered_full_path_with_url_operation_conventions(self):
-        self.assertEqual(
-            custom_filters.get_available_order_fields(
-                self.request, "operation_conventions"
-            ),
-            {},
-        )
-
-    def test_get_ordered_full_path_as_bailleur(self):
-        self.request.session["currently"] = GroupProfile.BAILLEUR
-        self.assertEqual(
-            custom_filters.get_available_order_fields(
-                self.request, "search_instruction"
-            ),
-            {
-                "programme__administration__nom": "Instructeur",
-                "programme__nom": "Opération",
-                "lot__financement": "Financement",
-                "programme__ville": "Ville",
-                "programme__code_postal": "Code postal",
-                "lot__nb_logements": "Nombre de logements",
-                "programme__date_achevement_compile": "Livraison",
-            },
-        )
-        self.assertEqual(
-            custom_filters.get_available_order_fields(self.request, "other_url_name"),
-            {
-                "programme__administration__nom": "Instructeur",
-                "numero": "Numéro",
-                "lot__financement": "Financement",
-                "programme__ville": "Ville",
-                "programme__code_postal": "Code postal",
-                "lot__nb_logements": "Nombre de logements",
-                "televersement_convention_signee_le": "Signature",
-            },
-        )
-
-    def test_get_ordered_full_path_as_instructeur(self):
-        self.request.session["currently"] = GroupProfile.INSTRUCTEUR
-        self.assertEqual(
-            custom_filters.get_available_order_fields(
-                self.request, "search_instruction"
-            ),
-            {
-                "programme__bailleur__nom": "Bailleur",
-                "programme__nom": "Opération",
-                "lot__financement": "Financement",
-                "programme__code_postal": "Code postal",
-                "programme__ville": "Ville",
-                "lot__nb_logements": "Nombre de logements",
-                "programme__date_achevement_compile": "Livraison",
-            },
-        )
-        self.assertEqual(
-            custom_filters.get_available_order_fields(self.request, "other_url_name"),
-            {
-                "programme__bailleur__nom": "Bailleur",
-                "numero": "Numéro",
-                "lot__financement": "Financement",
-                "programme__ville": "Ville",
-                "programme__code_postal": "Code postal",
-                "lot__nb_logements": "Nombre de logements",
-                "televersement_convention_signee_le": "Signature",
-            },
-        )
-
-
-class TestGetOrderValueFilters(TestCase):
-    def test_basic(self):
-        self.assertEqual(
-            custom_filters.get_ordervalue({"param1": "Params"}, "param1"), "Params"
-        )
-
-    def test_with_minus(self):
-        self.assertEqual(
-            custom_filters.get_ordervalue({"param1": "Params"}, "-param1"), "Params"
-        )
-
-    def test_no_key(self):
-        self.assertEqual(
-            custom_filters.get_ordervalue({"param1": "Params"}, "nokey"), ""
         )
