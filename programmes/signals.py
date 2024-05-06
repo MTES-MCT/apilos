dde915ccf398efe674edc68eb068bd9f0a2e018f
Sylvain Delabye <syl.delabye@gmail.com>
2024-05-06 14:54:06 +0200

    wip foyers

--- programmes/signals.py
+++ programmes/signals.py
@@ -15,7 +15,10 @@ def compute_date_achevement_compile(sender, instance, *args, **kwargs):
         instance.date_achevement or instance.date_achevement_previsible
     )
     if instance.code_postal and len(instance.code_postal) == 5:
-        code_departement = instance.code_postal[0:2]
+        if int(instance.code_postal) >= 97000:
+            code_departement = instance.code_postal[0:3]
+        else:
+            code_departement = instance.code_postal[0:2]
         if (
             instance.code_insee_departement != code_departement
             and instance.code_insee_departement
