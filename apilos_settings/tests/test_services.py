7b7fc676f725ba0958885dd52d81a0a5b5f25498
Nicolas Oudard <nicolas@oudard.org>
2024-05-13 15:10:11 +0200

    testlist views

--- apilos_settings/tests/test_services.py
+++ apilos_settings/tests/test_services.py
@@ -2,7 +2,15 @@ import pytest
 from django.contrib.messages.storage.fallback import FallbackStorage
 from django.test import RequestFactory
 
-from apilos_settings.services import user_is_staff_or_admin, user_profile
+from apilos_settings.services import (
+    administration_list,
+    bailleur_list,
+    user_is_staff_or_admin,
+    user_list,
+    user_profile,
+)
+from bailleurs.tests.factories import BailleurFactory
+from instructeurs.tests.factories import AdministrationFactory
 from users.forms import UserNotificationForm
 from users.tests.factories import UserFactory
 from users.type_models import EmailPreferences
@@ -63,3 +71,81 @@ class TestUserProfile:
         messages = list(messages)
         assert len(messages) == 1
         assert str(messages[0]) == "Votre profil a été enregistré avec succès"
+
+
+@pytest.mark.django_db
+class TestAdministrationList:
+
+    def test_admnistration_list_superuser(self):
+        AdministrationFactory()
+        factory = RequestFactory()
+        request = factory.get("/settings/administrations/")
+        user = UserFactory(is_superuser=True)
+        request.user = user
+
+        response = administration_list(request)
+        assert response["user_is_staff_or_admin"]
+        assert response["administrations"].paginator.count >= 1
+
+    def test_admnistration_list_staff(self):
+        AdministrationFactory()
+        factory = RequestFactory()
+        request = factory.get("/settings/administrations/")
+        user = UserFactory(is_staff=True)
+        request.user = user
+
+        response = administration_list(request)
+        assert response["user_is_staff_or_admin"]
+        assert response["administrations"].paginator.count >= 1
+
+
+@pytest.mark.django_db
+class TestBailleurList:
+
+    def test_bailleur_list_superuser(self):
+        BailleurFactory()
+        factory = RequestFactory()
+        request = factory.get("/settings/bailleurs/")
+        user = UserFactory(is_superuser=True)
+        request.user = user
+
+        response = bailleur_list(request)
+        assert response["user_is_staff_or_admin"]
+        assert response["total_items"] >= 1
+
+    def test_bailleur_list_staff(self):
+        BailleurFactory()
+        factory = RequestFactory()
+        request = factory.get("/settings/bailleurs/")
+        user = UserFactory(is_staff=True)
+        request.user = user
+
+        response = bailleur_list(request)
+        assert response["user_is_staff_or_admin"]
+        assert response["total_items"] >= 1
+
+
+@pytest.mark.django_db
+class TestUserList:
+
+    def test_user_list_superuser(self):
+        factory = RequestFactory()
+        request = factory.get("/settings/users/")
+        user = UserFactory(is_superuser=True)
+        UserFactory()
+        request.user = user
+
+        response = user_list(request)
+        assert response["user_is_staff_or_admin"]
+        assert response["total_users"] >= 1
+
+    def test_user_list_staff(self):
+        factory = RequestFactory()
+        request = factory.get("/settings/users/")
+        user = UserFactory(is_staff=True)
+        UserFactory()
+        request.user = user
+
+        response = user_list(request)
+        assert response["user_is_staff_or_admin"]
+        assert response["total_users"] >= 1
