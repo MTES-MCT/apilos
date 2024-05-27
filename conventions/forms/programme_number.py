451d4116b8ecabb08336cea37454651c94dbd90f
Nicolas Oudard <nicolas.oudard@beta.gouv.fr>
2024-05-27 10:24:39 +0200

    rename label 'Numero de décision' to 'Numéro d'opération' (#1442)
    * rename label 'Numero de décision' to 'Numéro d'opération'
    
    * Rename in forms labels
    
    ---------
    
    Co-authored-by: Sylvain Delabye <syl.delabye@gmail.com>

--- conventions/forms/programme_number.py
+++ conventions/forms/programme_number.py
@@ -3,10 +3,10 @@ from django import forms
 
 class ProgrammeNumberForm(forms.Form):
     numero_operation = forms.CharField(
-        label="N° de décision de financement",
+        label="Numéro d'opération",
         required=False,
         max_length=255,
         error_messages={
-            "max_length": "Le Numéro de décision ne doit pas excéder 255 caractères",
+            "max_length": "Le numero d'opération ne doit pas excéder 255 caractères",
         },
     )
