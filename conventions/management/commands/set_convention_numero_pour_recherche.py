49cb0cdf118efa4d6a925cbd5c23d0b5e22c8986
Nicolas Oudard <nicolas@oudard.org>
2024-06-04 06:50:53 +0200

    set convention numero_pour_recherche in command instead of migration

---
+++ conventions/management/commands/set_convention_numero_pour_recherche.py
@@ -0,0 +1,31 @@
+import logging
+
+from django.core.management.base import BaseCommand
+
+from conventions.models import Convention
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        conventions = (
+            Convention.objects.exclude(numero__isnull=True)
+            .exclude(numero="")
+            .order_by("id")
+        )
+        total = conventions.count()
+        i = 0
+        count = 0
+        while convs := conventions[i : i + 1000]:
+            logging.warning(f"{i}/{total}")
+            for c in convs:
+                c.numero_pour_recherche = (
+                    c.numero.replace("/", "")
+                    .replace("-", "")
+                    .replace(" ", "")
+                    .replace(".", "")
+                )
+                count += 1
+                c.save()
+            i += 1000
+        logging.warning(f"Enden : {i}/{count}")
