f0eabc4739fd259311c720bb1e959bfdeeb89c96
Nicolas Oudard <nicolas@oudard.org>
2024-08-05 11:09:24 +0200

    clean up unused email template

--- users/tests/test_services.py
+++ users/tests/test_services.py
@@ -1,7 +1,5 @@
 from django.test import TestCase
 
-from bailleurs.models import Bailleur
-from users.models import User
 from users.services import UserService
 
 
@@ -26,49 +24,3 @@ class UserServiceTest(TestCase):
 
         username = UserService.extract_username_from_email("vélomoteur")
         self.assertEqual(username, "")
-
-    def test_create_user_bailleur(self):
-        bailleur = Bailleur.objects.get(nom="HLM")
-
-        UserService.create_user_bailleur(
-            "Jeanne",
-            "Bailleur",
-            "jeanne.bailleur@apilos.com",
-            bailleur,
-            "jeanne.bailleur",
-            "http://apilos.dev/login",
-        )
-
-        user = User.objects.get(email="jeanne.bailleur@apilos.com")
-        self.assertEqual(user.first_name, "Jeanne")
-        self.assertEqual(user.last_name, "Bailleur")
-        self.assertTrue(user.is_bailleur(bailleur_id=bailleur.id))
-        self.assertEqual(user.username, "jeanne.bailleur")
-
-        UserService.create_user_bailleur(
-            "Jean",
-            "Bailleur",
-            "jean.bailleur2@apilos.com",
-            bailleur,
-            "jean.bailleur2",
-            "http://apilos.dev/login",
-        )
-        user = User.objects.get(email="jean.bailleur2@apilos.com")
-        self.assertEqual(user.first_name, "Jean")
-        self.assertEqual(user.last_name, "Bailleur")
-        self.assertTrue(user.is_bailleur(bailleur_id=bailleur.id))
-        self.assertEqual(user.username, "jean.bailleur2")
-
-        UserService.create_user_bailleur(
-            "Chantal",
-            "Bailleur",
-            "chantal.bailleur@apilos.com",
-            bailleur,
-            "chantal.bailleur3",
-            "http://apilos.dev/login",
-        )
-        user = User.objects.get(email="chantal.bailleur@apilos.com")
-        self.assertEqual(user.first_name, "Chantal")
-        self.assertEqual(user.last_name, "Bailleur")
-        self.assertTrue(user.is_bailleur(bailleur_id=bailleur.id))
-        self.assertEqual(user.username, "chantal.bailleur3")
