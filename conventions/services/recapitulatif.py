5b26c72ef5aa6312163e8e71af416d430d9be5c3
Nicolas Oudard <nicolas@oudard.org>
2024-10-08 10:43:58 +0200

    Envoyer les notif de correction aux bailleurs selon leur préférences

--- conventions/services/recapitulatif.py
+++ conventions/services/recapitulatif.py
@@ -374,7 +374,6 @@ def convention_feedback(request: HttpRequest, convention: Convention):
             cc,
             notification_form.cleaned_data["from_instructeur"],
             notification_form.cleaned_data["comment"],
-            all_bailleur_users=(notification_form.cleaned_data["all_bailleur_users"]),
         )
         target_status = ConventionStatut.INSTRUCTION.label
         if notification_form.cleaned_data["from_instructeur"]:
@@ -407,7 +406,6 @@ def send_email_correction(
     cc,
     from_instructeur,
     comment=None,
-    all_bailleur_users=False,
 ):
     """
     send email to notify correction is needed of correction is done:
@@ -418,7 +416,7 @@ def send_email_correction(
 
     if from_instructeur:
         # Get bailleurs list following email preferences and interaction with the convention
-        to = convention.get_email_bailleur_users(all_bailleur_users=all_bailleur_users)
+        to = convention.get_email_bailleur_users()
         if convention.is_avenant():
             email_template_id = EmailTemplateID.ItoB_AVENANT_CORRECTIONS_REQUISES
         else:
