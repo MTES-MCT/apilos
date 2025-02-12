e88f8e7b376f487af3f579bd7817229c41df20e2
Etchegoyen Matthieu <etchegom@gmail.com>
2025-02-12 10:48:31 +0100

    fix test

--- conventions/services/convention_generator.py
+++ conventions/services/convention_generator.py
@@ -625,9 +625,10 @@ def _get_residence_attributions(convention: Convention) -> str:
 def _get_foyer_attributions(convention: Convention) -> str:
     if not convention.programme.is_foyer:
         return ""
-    if attribution_type := convention.attribution_type is None:
-        return ""
-    return foyer_attributions_mapping.get(attribution_type, "")
+    attribution_type = convention.attribution_type
+    return (
+        foyer_attributions_mapping.get(attribution_type, "") if attribution_type else ""
+    )
 
 
 def fiche_caf_doc(convention):
