08d7235046cb0f4652766bbc0ac7d36ba66f01fc
Nicolas Oudard <nicolas@oudard.org>
2025-04-08 18:07:51 +0200

    Suppression de l'envoi de l'email mensuel

--- core/worker.py
+++ core/worker.py
@@ -1,7 +1,6 @@
 import os
 
 from celery import Celery
-from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
@@ -16,10 +15,3 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
-
-app.conf.beat_schedule = {
-    "send-monthly-emails": {
-        "task": "users.tasks.send_monthly_emails",
-        "schedule": crontab(minute=0, hour=7, day_of_week=1),
-    },
-}
