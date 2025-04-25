97532f8c7b37281dfd32be181c440d3700cd2426
Sylvain Delabye <syl.delabye@gmail.com>
2025-04-25 11:45:21 +0200

    Avenants variantes et attribution (#1843)

--- conventions/urls.py
+++ conventions/urls.py
@@ -177,6 +177,21 @@ urlpatterns = [
         name="avenant_collectif",
     ),
     path(
+        "avenant_foyer_attribution/<convention_uuid>",
+        views.AvenantFoyerAttributionView.as_view(),
+        name="avenant_foyer_attribution",
+    ),
+    path(
+        "avenant_residence_attribution/<convention_uuid>",
+        views.AvenantResidenceAttributionView.as_view(),
+        name="avenant_residence_attribution",
+    ),
+    path(
+        "avenant_variantes/<convention_uuid>",
+        views.AvenantFoyerVariantesView.as_view(),
+        name="avenant_variantes",
+    ),
+    path(
         "stationnements/<convention_uuid>",
         views.ConventionTypeStationnementView.as_view(),
         name="stationnements",
