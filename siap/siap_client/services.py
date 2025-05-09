2191406daf8333e8695b1e454512982842f47a0b
Etchegoyen Matthieu <etchegom@gmail.com>
2025-05-09 09:47:13 +0200

    WIP

--- siap/siap_client/services.py
+++ siap/siap_client/services.py
@@ -6,7 +6,7 @@ from .client import SIAPClient
 from .schemas import Alerte
 
 
-def get_siap_credentials(request: HttpRequest) -> tuple[str, int]:
+def siap_credentials_from_request(request: HttpRequest) -> dict[str, Any]:
     """
     Get the user login and habilitation ID from the request.
     """
@@ -16,22 +16,21 @@ def get_siap_credentials(request: HttpRequest) -> tuple[str, int]:
     if not user_login or not habilitation_id:
         raise ValueError("user_login and habilitation_id are required")
 
-    return user_login, habilitation_id
+    return {
+        "user_login": user_login,
+        "habilitation_id": habilitation_id,
+    }
 
 
 def create_siap_alerte(alerte: Alerte, request: HttpRequest) -> dict[str, Any]:
-    user_login, habilitation_id = get_siap_credentials(request)
     return SIAPClient.get_instance().create_alerte(
-        user_login=user_login,
-        habilitation_id=habilitation_id,
         payload=alerte.to_json(),
+        **siap_credentials_from_request(request),
     )
 
 
 def delete_siap_alerte(alerte_id: str, request: HttpRequest) -> dict[str, Any]:
-    user_login, habilitation_id = get_siap_credentials(request)
     return SIAPClient.get_instance().delete_alerte(
-        user_login=user_login,
-        habilitation_id=habilitation_id,
         alerte_id=alerte_id,
+        **siap_credentials_from_request(request),
     )
