0e51e46578021558fa92e7d24ead4d77f1f72147
Ayoub BOUCHACHIA <ayoub.bouchachia@sully-group.com>
2025-09-22 10:28:32 +0200

    Ajout d’un feature flag permettant d’activer/désactiver les conventions mixtes

--- conventions/views/conventions_mix.py
+++ conventions/views/conventions_mix.py
@@ -1,15 +1,18 @@
-import logging
-
+from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import View
+from waffle import switch_is_active
 
 from conventions.forms.convention_mixed_form_initialisation import UUIDListForm
 from conventions.models.convention import Convention
 
+
 class ConventionMix(View):
 
     def post(self, request):
+        if not switch_is_active(settings.SWITCH_CONVENTION_MIXTE_ON):
+            return HttpResponseRedirect(reverse("conventions:search"))
         form = UUIDListForm(request.POST)
         if form.is_valid():
             uuids = form.cleaned_data["uuids"]
@@ -18,7 +21,10 @@ class ConventionMix(View):
             if action == "create":
                 _, _, convention_mixte = Convention.objects.group_conventions(uuids)
                 return HttpResponseRedirect(
-                    reverse("programmes:operation_conventions", args=[convention_mixte.programme.numero_operation])
+                    reverse(
+                        "programmes:operation_conventions",
+                        args=[convention_mixte.programme.numero_operation],
+                    )
                 )
             elif action == "dispatch":
                 conventions = Convention.objects.degroup_conventions(uuids)
