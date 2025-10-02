5d19c16d99f9a9f66d9523a159503d949e5679ce
Ayoub BOUCHACHIA <ayoub.bouchachia@sully-group.com>
2025-10-02 18:39:08 +0200

    fix Coverage

--- conventions/views/conventions_mix.py
+++ conventions/views/conventions_mix.py
@@ -9,14 +9,18 @@ from conventions.models.convention import Convention
 
 
 class ConventionMix(View):
+    """
+    View to handle grouping or degrouping of conventions
+    based on UUIDs and the requested action ('create' or 'dispatch').
+    """
 
     def post(self, request):
         if not switch_is_active(settings.SWITCH_CONVENTION_MIXTE_ON):
             return HttpResponseRedirect(reverse("conventions:search"))
         form = UUIDListForm(request.POST)
         if form.is_valid():
-            uuids = form.cleaned_data["uuids"]
-            action = form.cleaned_data["action"]
+            uuids = form.cleaned_data.get("uuids", [])
+            action = form.cleaned_data.get("action")
 
             if action == "create":
                 _, _, convention_mixte = Convention.objects.group_conventions(uuids)
@@ -26,14 +30,16 @@ class ConventionMix(View):
                         args=[convention_mixte.programme.numero_operation],
                     )
                 )
-            elif action == "dispatch":
+            if action == "dispatch":
                 conventions = Convention.objects.degroup_conventions(uuids)
-                return HttpResponseRedirect(
-                    reverse(
-                        "programmes:operation_conventions",
-                        args=[conventions.first().programme.numero_operation],
+                first_convention = conventions.first()
+                if first_convention:
+                    return HttpResponseRedirect(
+                        reverse(
+                            "programmes:operation_conventions",
+                            args=[first_convention.programme.numero_operation],
+                        )
                     )
-                )
 
         # If form is invalid, redirect to search
         return HttpResponseRedirect(reverse("conventions:search"))
