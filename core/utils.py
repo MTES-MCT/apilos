37be46accb1b0064c8b5ca500550f4077efaeda4
Ayoub BOUCHACHIA <bouchachiaayoub7@gmail.com>
2026-09-25 14:16:28 +0200

    add log error

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import unicodedata
 import uuid
 from typing import Any, SupportsRound
@@ -6,6 +7,8 @@ from typing import Any, SupportsRound
 from django.conf import settings
 from django.core.exceptions import PermissionDenied
 from django.utils.crypto import get_random_string
+
+logger = logging.getLogger(__name__)
 
 
 def round_half_up(number: SupportsRound[Any], ndigits: int = 0):
@@ -68,6 +71,9 @@ def check_user_whitelist(user):
             login.strip() for login in allowed_logins_str.split(",") if login.strip()
         ]
         if user.cerbere_login not in allowed_logins:
+            logger.error(
+                f"Accès rejeté par la liste blanche pour le login: {user.cerbere_login}"
+            )
             raise PermissionDenied(
                 "Votre compte n'est pas autorisé à accéder à cette application."
             )
